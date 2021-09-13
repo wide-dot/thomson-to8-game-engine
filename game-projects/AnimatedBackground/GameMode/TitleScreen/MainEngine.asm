d6828eda27457fc06aa1939b23e2a46a48c91bda
François-Xavier Robin <fxrobin@gmail.com>
2021-09-13 15:52:58 +0200

    import de AnimatedBackground

---
+++ game-projects/AnimatedBackground/GameMode/TitleScreen/MainEngine.asm
@@ -0,0 +1,55 @@
+        opt   c,ct
+
+********************************************************************************
+* Game Engine (TO8 Thomson) - Benoit Rousseau 2020-2021
+* ------------------------------------------------------------------------------
+*
+*
+********************************************************************************
+
+        INCLUDE "./Engine/Constants.asm"
+        INCLUDE "./Engine/Macros.asm"        
+        org   $6100
+
+* ==============================================================================
+* Main Loop
+* ==============================================================================
+LevelMainLoop
+        jsr   WaitVBL    
+        jsr   UpdatePalette
+        jsr   RunObjects
+        jsr   CheckSpritesRefresh                                              
+        jsr   EraseSprites
+        jsr   UnsetDisplayPriority
+        jsr   DrawSprites       
+        bra   LevelMainLoop
+        
+* ---------------------------------------------------------------------------
+* Game Mode RAM variables
+* ---------------------------------------------------------------------------
+        
+        INCLUDE "./GameMode/TitleScreen/TitleScreenRamData.asm"        
+
+* ==============================================================================
+* Routines
+* ==============================================================================
+		INCLUDE "./Engine/Ram/BankSwitch.asm"
+        INCLUDE "./Engine/Graphics/WaitVBL.asm"
+        INCLUDE "./Engine/Graphics/DrawFullscreenImage.asm"	
+        INCLUDE "./Engine/Graphics/AnimateSprite.asm"	
+        INCLUDE "./Engine/Graphics/GetImgIdA.asm"
+        INCLUDE "./Engine/Graphics/DisplaySprite.asm"	
+        INCLUDE "./Engine/Graphics/CheckSpritesRefresh.asm"
+        INCLUDE "./Engine/Graphics/EraseSprites.asm"
+        INCLUDE "./Engine/Graphics/UnsetDisplayPriority.asm"
+        INCLUDE "./Engine/Graphics/DrawSprites.asm"
+        INCLUDE "./Engine/Graphics/BgBufferAlloc.asm"	
+        INCLUDE "./Engine/Joypad/ReadJoypads.asm"
+        INCLUDE "./Engine/ObjectManagement/RunObjects.asm"
+        INCLUDE "./Engine/ObjectManagement/DeleteObject.asm"
+        INCLUDE "./Engine/ObjectManagement/ClearObj.asm"
+        INCLUDE "./Engine/ObjectManagement/RunPgSubRoutine.asm"	
+        INCLUDE "./Engine/LevelManagement/LoadGameMode.asm"
+        INCLUDE "./Engine/Palette/UpdatePalette.asm"
+        INCLUDE "./Engine/Sound/PSGlib.asm"
+        INCLUDE "./Engine/Irq/IrqPsgJoypad.asm"	
---
+++ game-projects/AnimatedBackground/GameMode/TitleScreen/TitleScreenRamData.asm
@@ -0,0 +1,18 @@
+; RAM variables
+
+* ===========================================================================
+* Object Constants
+* ===========================================================================
+
+nb_dynamic_objects                equ 2
+nb_graphical_objects              equ 2 * max 64 total
+
+Object_RAM
+SS_Object_RAM
+Dynamic_Object_RAM                fcb   ObjID_Coffee
+                                  fill  0,object_size-1
+                                  fcb   ObjID_Player
+                                  fill  0,object_size-1
+Dynamic_Object_RAM_End
+SS_Object_RAM_End
+Object_RAM_End
