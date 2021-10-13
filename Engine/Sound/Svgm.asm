7457299c3a824ec93e09da4e6c75174ebef516e7
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2021-10-13 11:58:48 +0200

    Add new Svgm sound driver

---
+++ Engine/Sound/Svgm.asm
@@ -0,0 +1,151 @@
+* ---------------------------------------------------------------------------
+* SVGM 6809 - Small VGM playback system for 6809
+* ---------------------------------------------------------------------------
+* by Bentoc October 2021
+* ---------------------------------------------------------------------------
+
+	opt   c,ct
+
+; Hardware Addresses
+PSG             equ   $E7FF
+YM2413_A0       equ   $E7FC
+YM2413_D0       equ   $E7FD
+
+MusicPage       fcb   0                ; memory page of music data
+MusicData       fdb   0                ; address of song data
+MusicDataPos    fdb   0                ; current playing position in Music Data
+MusicStatus     fcb   0                ; 0 : stop playing, 1-255 : play music
+WaitFrame       fcb   0                ; number of frames to wait before next play
+
+******************************************************************************
+* PlayMusic - Load a new music and init all tracks
+*
+* receives in X the address of the song
+* destroys X
+******************************************************************************
+
+PlayMusic
+        pshs  d
+        lda   ,x                       ; get memory page that contains track data
+        sta   MusicPage
+	sta   MusicStatus
+	lda   #1
+	sta   WaitFrame
+        ldx   1,x                      ; get ptr to track data
+        stx   MusicData
+	stx   MusicDataPos
+        puls  d,pc	
+
+******************************************************************************
+* MusicFrame - processes a music frame (VInt)
+*
+* format:
+* -------
+* x00-x38 xnn           : (2 bytes) YM2413
+* x40-xff               : (1 byte)  SN76489
+* x39     xnn           : (2 bytes) command code xnn (x00-xff)
+*   |____ x00           : (2 bytes) stop track
+*   |____ xnn           : (2 bytes) wait xnn frames (x01-x7f)
+*   |____ x80 xnnnn     : (4 bytes) jump to xnnnn 
+*   |____ x81 xnnnn xnn : (5 bytes) replay at xnnnn (signed offset) xnn bytes and return
+*
+* destroys A,B,X,Y
+******************************************************************************
+        
+MusicFrame 
+        lda   MusicStatus
+        bne   @a
+	rts    
+@a      lda   WaitFrame
+        deca
+	sta   WaitFrame
+        beq   UpdateMusic
+        rts
+
+UpdateMusic
+        lda   MusicPage
+        bne   @a
+	rts                            ; no music to play
+@a      _SetCartPageA
+        ldx   MusicDataPos
+
+UpdateLoop
+	lda   ,x+
+	cmpa  #$39
+	beq   DoCommand
+	blo   YM2413
+
+SN76489	
+        sta   <PSG
+	bra   UpdateLoop
+
+DoCommand
+	lda   ,x+
+	beq   DoStopTrack
+	bmi   DoSpecialCommand
+        
+DoWait
+	sta   WaitFrame
+	stx   MusicDataPos
+
+DoSpecialCommand
+        rts	
+
+DoStopTrack
+        lda   #0
+	sta   MusicStatus
+        lda   #1
+	sta   WaitFrame
+        ldx   MusicData
+	stx   MusicDataPos	
+	jsr   PSGSilenceAll
+	jsr   FMSilenceAll
+	rts
+       	
+YM2413
+        sta   <YM2413_A0
+        ldb   ,x+
+        stb   <YM2413_D0
+	nop
+	nop                            ; tempo (should be 24 cycles between two register writes)
+	bra   UpdateLoop
+
+******************************************************************************
+* PSGSilenceAll
+* destroys A
+******************************************************************************
+        
+PSGSilenceAll
+        lda   #$9F
+        sta   PSG
+        lda   #$BF
+        sta   PSG       
+        lda   #$DF
+        sta   PSG
+        lda   #$FF
+        sta   PSG                               
+        rts  
+
+******************************************************************************
+* FMSilenceAll
+* destroys A, B, Y
+******************************************************************************
+
+FMSilenceAll
+        ldd   #$200E
+        stb   YM2413_A0
+        nop                            ; (wait of 2 cycles)
+	ldb   #0                       ; (wait of 2 cycles)
+        sta   YM2413_D0                ; note off for all drums     
+
+        lda   #$20                     ; (wait of 2 cycles)
+	brn   *                        ; (wait of 3 cycles)
+@a      exg   a,b                      ; (wait of 8 cycles)                      		
+        exg   a,b                      ; (wait of 8 cycles)                      		
+        sta   YM2413_A0
+        nop
+        inca
+        stb   YM2413_D0
+        cmpa  #$29                     ; (wait of 2 cycles)
+        bne   @a                       ; (wait of 3 cycles)
+        rts	
---
+++ java-generator/src/main/java/fr/bento8/to8/audio/SVGMTool/vgm/VGMInterpreter.java
@@ -0,0 +1,161 @@
+package fr.bento8.to8.audio.SVGMTool.vgm;
+
+import java.io.File;
+import java.io.IOException;
+
+import fr.bento8.to8.audio.SVGMTool.SVGMTool;
+
+public class VGMInterpreter {
+	public static final int SAMPLES_PER_SECOND = 44100;
+	public static final int SAMPLES_PER_FRAME_NTSC = 735;
+	public static final int SAMPLES_PER_FRAME_PAL = 882;
+
+	private VGMInputStream input;
+	public int[] arrayOfInt = new int[16384];
+	public int i = 0;
+	public int loopMarkerHit = 0;
+	public int cumulatedFrames = 0;
+	public int[] drum = new int[]{0x30, 0x28, 0x21, 0x22, 0x24};
+
+	public VGMInterpreter(File paramFile) throws IOException {
+		this.input = new VGMInputStream(paramFile);
+		while(run()) {
+		}
+	}
+
+	public boolean run() throws IOException {
+		boolean skipFrame = false;
+
+		while (!skipFrame) {
+			if (this.input.isLoopPoint())
+				fireLoopPointHit(); 
+			int i = this.input.read();
+
+			switch (i) {
+			case 0x4F: // Game Gear PSG stereo, write dd to port 0x06
+				this.input.read();
+				continue;
+			case 0x50: // PSG (SN76489/SN76496) write value dd
+				int val = this.input.read();
+				if (val <= 127 )
+					val = val | 0b01000000;
+				fireWrite(val);
+				continue;
+			case 0x51: // YM2413, write value dd to register aa
+				int cmd = this.input.read();
+				int data = this.input.read();
+
+				// Overwrite drum volume
+				if (cmd==0x36)
+					data = SVGMTool.drumVol[0];
+				if (cmd==0x37)
+					data = SVGMTool.drumVol[1];
+				if (cmd==0x38)
+					data = SVGMTool.drumVol[2];
+				
+				fireWrite(cmd);
+				fireWrite(data);
+				
+				continue;          				
+			case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57: case 0x58: case 0x59: case 0x5A: case 0x5B: case 0x5C: case 0x5D: case 0x5E: case 0x5F:        	
+				this.input.skip(2L); // write value dd to register aa
+				continue;          								
+			case 0x61: // Wait n samples, n can range from 0 to 65535
+				int waitTime = this.input.readShort();
+				if (waitTime != 882) {
+					close();
+					throw new IOException("0x61 VGM command not supported at " + Integer.toHexString(this.input.getPosition() - 3) + ": " + Integer.toHexString(waitTime) + "\nOnly PAL wait frame is supported either 0x61 0x72 0x03 or 0x63 command");
+				}
+            	cumulatedFrames++;				
+				skipFrame = true;
+				continue;
+			case 0x62: // wait 735 samples (60th of a second)
+				close();
+				throw new IOException("0x62 VGM command not supported at " + Integer.toHexString(this.input.getPosition() - 3) + "\nOnly PAL wait frame is supported either 0x61 0x72 0x03 or 0x63 command");
+            case 0x63: // wait 882 samples (50th of a second)
+            	cumulatedFrames++;
+				skipFrame = true;
+				continue;
+			case 0x66: // end of sound data
+				fireWriteEnd();				
+				close();
+				return false;
+			case 0x67: // data block
+				continue;				
+			case 0x80: case 0x81: case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87: case 0x88: case 0x89: case 0x8A: case 0x8B: case 0x8C: case 0x8D: case 0x8E: case 0x8F:
+				this.input.skip(1L);
+				continue;
+			case 0x90: // DAC Stream Control Write - Setup Stream Control
+				this.input.skip(4L);
+				continue;
+			case 0x91: // DAC Stream Control Write - Set Stream Data
+				this.input.skip(4L);
+				continue;
+			case 0x92: // DAC Stream Control Write - Set Stream Frequency
+				this.input.skip(5L);
+				continue;
+			case 0x93: // DAC Stream Control Write - Start Stream
+				this.input.skip(10L);
+				continue;
+			case 0x94: // DAC Stream Control Write - Stop Stream
+				this.input.skip(1L);
+				continue;
+			case 0x95: // DAC Stream Control Write - Start Stream (fast call)
+				fireWrite(0x0E);
+				fireWrite(0x20);
+				fireWrite(0x0E);
+				this.input.read();      // Stream id
+				int blockId = this.input.readShort(); // Block id
+				fireWrite(drum[blockId%drum.length]);
+				this.input.skip(1L);    // skip Flags
+				continue;								
+			case 0xE0:        	
+				this.input.skip(4L); // data reg
+				continue;               
+			} 
+			close();
+			throw new IOException("Unknown VGM command encountered at " + Integer.toHexString(this.input.getPosition() - 1) + ": " + Integer.toHexString(i));
+		} 
+
+		return true;
+	}
+
+	public int getTotalSamples() {
+		return this.input.getTotalSamples();
+	}
+
+	public void close() throws IOException {
+		this.input.close();
+	}
+
+	public void fireLoopPointHit() {
+		loopMarkerHit = i;
+	}
+	
+	public void fireWrite(int paramInt) {
+		while (cumulatedFrames > 0) {
+			arrayOfInt[i++] = 0x39;
+			if (cumulatedFrames > 127) {
+				arrayOfInt[i++] = cumulatedFrames;
+				cumulatedFrames -= 127;
+			} else {
+				arrayOfInt[i++] = cumulatedFrames;
+				cumulatedFrames = 0;
+			}
+		}
+		arrayOfInt[i++] = paramInt;
+	}
+	
+	public void fireWriteEnd() {
+		arrayOfInt[i++] = 0x39;
+		arrayOfInt[i++] = 0x00;
+	}
+	
+	public int[] getArrayOfInt() {
+		return arrayOfInt;
+	}
+	
+	public int getLastIndex() {
+		return i;
+	}	
+}
