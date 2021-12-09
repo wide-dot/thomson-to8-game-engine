faec3ab1d251317ba076e9a9db66aea26cdc97df
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2021-12-09 15:01:54 +0100

    Add FIRQ support for midi extension

--- Engine/Sound/Smidi.asm
+++ Engine/Sound/Smidi.asm
@@ -1,5 +1,5 @@
 * ---------------------------------------------------------------------------
-* SMIDI 6809 - Small Midi playback system for 6809
+* SMIDI 6809 - Small Midi playback system for 6809 and EF5860
 * ---------------------------------------------------------------------------
 * by Bentoc October 2021
 * with inputs from Fool-DupleX
@@ -7,13 +7,16 @@
 
     opt   c,ct
 
-MIDI.CTRL       EQU    $E7F2
-MIDI.STAT       EQU    MIDI.CTRL
-MIDI.RX         EQU    $E7F3
-MIDI.TX         EQU    MIDI.RX
-MIDI.RXFULL     EQU    $01
-MIDI.TXEMPTY    EQU    $02
-MIDI.RXIRQ      EQU    $80
+MIDI.CTRL       equ    $E7F2
+MIDI.STAT       equ    MIDI.CTRL
+MIDI.RX         equ    $E7F3
+MIDI.TX         equ    MIDI.RX
+MIDI.RXFULL     equ    %00000001
+MIDI.TXEMPTY    equ    %00000010
+MIDI.RXIRQ      equ    %10000000
+MIDI.TXIRQ      equ    %00100000
+
+FIRQ.ROUTINE    equ    $6023
 
 MusicLoop       fcb   0                ; 0 : do not loop music
 MusicIndex      fdb   0                ; first index of all data chuncks (page, address)
@@ -44,25 +47,29 @@ ResetMidi
 ******************************************************************************
 
 PlayMusicLoop
-        pshs  a
+        pshs  d
         lda   #$ff
         sta   MusicLoop                ; set the loop flag on
         bra   @a
 PlayMusic
-        pshs  a
+        pshs  d
 @a      stx   MusicIndex               ; store index for loop restart
         stx   MusicIndexPos            ; init data chunck index position
         lda   sound_page,x             ; get memory page that contains track data
         sta   MusicPage
         sta   MusicStatus              ; no data on page 0, page is used to init status to a non zero value
-        lda   #1
-        sta   WaitFrame                ; wait frame is only zero where reading commands, should be init to 1
+        ldd   #1
+        stb   WaitFrame                ; wait frame is only zero where reading commands, should be init to 1
         ldx   sound_start_addr,x       ; get ptr to track data
         stx   MusicDataPos             ; init data location
-        puls  a,pc
+	sta   CircularBufferEnd+1      ; init buffer index to 0
+	sta   CircularBufferPos+2      ; init buffer index to 0
+	ldd   #SampleFIRQ              ; set the FIRQ routine that will be called when the EF5860 tx buffer is empty
+	std   FIRQ.ROUTINE
+        puls  d,pc
 
 ******************************************************************************
-* MusicFrame - processes a music frame (VInt - DP $E7)
+* MusicFrame - processes a music frame (IRQ - DP $E7)
 *
 * format:
 * -------
@@ -72,8 +79,20 @@ PlayMusic
 *   |____ x80-xff xnn (xnn) : (2 or 3 bytes) command, data1, (data2)
 *   |____ xf0 xnn ... xf7   : (x bytes) SysEx
 *
-* destroys A,B,X
+* destroys A,B,X,Y
 ******************************************************************************
+_enableFIRQ MACRO
+	lda   MIDI.CTRL
+	anda  #^MIDI.TXIRQ
+        sta   MIDI.CTRL
+ ENDM 
+
+_writeBuffer MACRO
+        sta   b,y                      ; send byte to the midi interface
+	incb
+	stb   CircularBufferEnd+1
+        lda   ,x+
+ ENDM 
 
 MusicFrame 
         lda   MusicStatus              ; check if a music track is to play
@@ -107,56 +126,73 @@ LoopRestart
 	bra   @a
         ;
 ReadCommand
+        ldb   CircularBufferEnd+1      ; load next free position in circular buffer
+	ldy   #CircularBuffer
         ldx   MusicDataPos             ; load current position in music track
         lda   MusicPage
 @a      _SetCartPageA
-    
-CommandLoop
         lda   ,x+                      ; read data byte in new page
+
+CommandLoop
         bmi   DoCommand
         beq   BankSwitch
 
 DoWait
         sta   WaitFrame
         stx   MusicDataPos
+	_enableFIRQ
         rts
 
 DoCommand
         cmpa  #$C0
-        blo   TXRdyLoop0               ; skip 2 bytes (command 8n to Bn, 2 data byte)
+        blo   TXRdy3b                  ; branch (command 8n to Bn, 2 data bytes)
         cmpa  #$E0                
-        blo   TXRdyLoop1               ; skip 1 byte  (command Cn and Dn, 1 data byte)
+        blo   TXRdy2b                  ; branch (command Cn and Dn, 1 data byte)
         cmpa  #$f0
-        beq   DoSysEx
-                                       ; bne: skip 2 bytes (command En, 2 data byte)
-TXRdyLoop0
-        ldb   <MIDI.STAT               ; wait interface to be ready
-        andb  #MIDI.TXEMPTY
-        beq   TXRdyLoop0              
-        sta   <MIDI.TX                 ; send byte to the midi interface
-        lda   ,x+
+        beq   DoSysEx                  ; branch (sysex)
+                                       ; default (command En, 2 data byte)
 
-TXRdyLoop1             
-        ldb   <MIDI.STAT               ; wait interface to be ready
-        andb  #MIDI.TXEMPTY
-        beq   TXRdyLoop1             
-        sta   <MIDI.TX                 ; send byte to the midi interface
-        lda   ,x+
+TXRdy3b                             
+        _writeBuffer
 
-TXRdyLoop2             
-        ldb   <MIDI.STAT               ; wait interface to be ready
-        andb  #MIDI.TXEMPTY
-        beq   TXRdyLoop2              
-        sta   <MIDI.TX                 ; send byte to the midi interface
-        bra  CommandLoop   
+TXRdy2b
+        _writeBuffer
+        _writeBuffer
+        bra   CommandLoop
 
 DoSysEx
-        ldb   <MIDI.STAT               ; wait interface to be ready
-        andb  #MIDI.TXEMPTY
-        beq   DoSysEx              
-        sta   <MIDI.TX                 ; send byte to the midi interface
-        lda   ,x+
+        _writeBuffer
         cmpa  #$f7
         bne   DoSysEx
-        sta   <MIDI.TX                 ; send byte to the midi interface           
-        bra  CommandLoop
+        _writeBuffer
+        bra   CommandLoop
+
+******************************************************************************
+* SampleFIRQ - send a sample to the midi interface (FIRQ)
+*
+******************************************************************************
+
+SampleFIRQ
+        sta   @a+1                     ; backup register A
+	lda   CircularBufferPos+2      ; read current offset in buffer
+CircularBufferEnd
+	cmpa  #0                       ; (dynamic) end offset in buffer (set by IRQ routine when buffer is written)
+	beq   DisableFIRQ             ; branch if no more data to read (todo shutdown midi interface interupt until next buffer write ?)
+	inc   CircularBufferPos+2      ; increment the offset in buffer
+CircularBufferPos	
+        lda   CircularBuffer           ; (dynamic) read the buffer at the current index
+        sta   MIDI.TX                  ; send byte to the midi interface           
+@a      lda   #0                       ; (dynamic) restore register A
+        rti
+DisableFIRQ
+	lda   MIDI.CTRL
+	anda  #^MIDI.TXIRQ
+        sta   MIDI.CTRL
+        rti
+
+        align 256
+CircularBuffer
+        fill  0,256
+
+; todo IRQ routine must record the byte in buffer and then increment the last index, FIRQ is able to run simultaneously
+; todo at encoding stage a frame is not allowed to hold more than 62 midi bytes, place a warning
