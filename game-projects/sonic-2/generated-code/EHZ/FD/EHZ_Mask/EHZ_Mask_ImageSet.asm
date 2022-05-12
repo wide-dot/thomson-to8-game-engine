3a6a4262c889836e58b82a233b4a58e326e2c2fa
Benoît Rousseau <56814489+bhrousseau@users.noreply.github.com>
2022-05-12 17:03:09 +0200

    Delete game-projects/sonic-2/generated-code directory

--- game-projects/sonic-2/generated-code/EHZ/FD/EHZ_Mask/EHZ_Mask_ImageSet.asm
+++
@@ -1,8 +0,0 @@
-	org   $3FF0
-	setdp $FF
-
-* Generated Code
-
-Img_EHZ_Mask 
-        fcb   $07,$07,$07,$07,$93,$BA,$00,$00,$06,$00,$00,$B6,$A3,$78
-        fcb   $31,$4D
--- game-projects/sonic-2/generated-code/TitleScreen/FD/SonicAndTailsIn/SonicAndTailsIn_ImageSet.asm
+++
@@ -1,8 +0,0 @@
-	org   $3F0A
-	setdp $FF
-
-* Generated Code
-
-Img_SonicAndTailsIn 
-        fcb   $07,$07,$07,$07,$88,$4F,$00,$00,$06,$00,$00,$BB,$D9,$6A
-        fcb   $30,$E1
