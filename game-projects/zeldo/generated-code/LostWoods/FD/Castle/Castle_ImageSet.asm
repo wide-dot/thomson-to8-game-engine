4d485a021ede3902b5cb1d1277ff5c777a1ac980
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2022-07-24 16:24:56 +0200

    dummy

--- game-projects/zeldo/generated-code/LostWoods/FD/Castle/Castle_ImageSet.asm
+++
@@ -1,8 +0,0 @@
-	org   $14C3
-	setdp $FF
-
-* Generated Code
-
-Img_Castle 
-        fcb   $07,$07,$07,$07,$9F,$8F,$00,$00,$06,$00,$00,$B0,$B9,$7C
-        fcb   $00,$00
--- game-projects/zeldo/generated-code/Triangle/Img_0004_0_ND0.asm
+++
@@ -1,9 +0,0 @@
-	INCLUDE "./engine/constants.asm"
-	ORG $A000
-	SETDP $FF
-	OPT C,CT
-DRAW_Img_0004_0
-
-	LDU <glb_screen_location_1
-	RTS
-
