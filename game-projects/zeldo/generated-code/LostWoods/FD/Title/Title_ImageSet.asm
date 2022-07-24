e5d17e499ea81a5dd8c29f1e9a9f762b4fe4ec54
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2022-07-24 16:24:56 +0200

    dummy

--- game-projects/zeldo/generated-code/LostWoods/FD/Title/Title_ImageSet.asm
+++
@@ -1,8 +0,0 @@
-	org   $1451
-	setdp $FF
-
-* Generated Code
-
-Img_Title 
-        fcb   $07,$07,$07,$07,$69,$53,$01,$00,$06,$00,$00,$CC,$D7,$7D
-        fcb   $00,$00
--- game-projects/zeldo/generated-code/LostWoods/T2/Title/Title_ImageSet.asm
+++
@@ -1,8 +0,0 @@
-	org   $0647
-	setdp $FF
-
-* Generated Code
-
-Img_Title 
-        fcb   $07,$07,$07,$07,$69,$53,$01,$00,$06,$00,$00,$CC,$D7,$90
-        fcb   $00,$00
