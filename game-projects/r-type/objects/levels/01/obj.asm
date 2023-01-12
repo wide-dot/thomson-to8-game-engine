c224d7e875fb0166f5c68599ecd833b340312c87
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-01-13 00:31:40 +0100

    wip on horizontal scroll

--- game-projects/r-type/objects/levels/01/obj.asm
+++ game-projects/r-type/objects/levels/01/obj.asm
@@ -19,13 +19,49 @@ Level_Init_0
         lda   render_flags,u
         ora   #render_overlay_mask
         sta   render_flags,u
-	inc   routine,u
-        jmp   DisplaySprite	
+
+        ; register map location to main
+        ldx   #scroll_map
+        ldd   #Tls_lvl01
+        std   ,x
+        ldd   #Tls_lvl01_s
+        std   2,x
+        ldx   #scroll_map_page
+        _GetCartPageA
+        sta   ,x
+        std   1,x
+
+        ; set scroll parameters
+        ldd   #10
+        std   scroll_vp_h_tiles
+        ldd   #12
+        std   scroll_vp_v_tiles
+        lda   #14
+        sta   scroll_tile_width
+        lda   #10
+        sta   scroll_vp_x_pos
 
 Level_Init_1
 	inc   routine,u
-        jmp   DisplaySprite
+        jmp   DisplaySprite	
 
 Level_End
         jmp   DeleteObject
 
+        align 256
+Tls_lvl01
+        INCLUDEGEN Tls_lvl01 buffer
+        ; pre-rendered tilemap buffer
+        ; free    (byte) reserved for future use - can be used for layer information
+        ; page    (byte) page number of compilated tile routine
+        ; address (word) absolute address of compilated tile routine
+        ; [repeated for each tile in the map]
+
+        align 256
+Tls_lvl01_s
+        INCLUDEGEN Tls_lvl01_s buffer
+        ; pre-rendered tilemap buffer
+        ; free    (byte) reserved for future use - can be used for layer information
+        ; page    (byte) page number of compilated tile routine
+        ; address (word) absolute address of compilated tile routine
+        ; [repeated for each tile in the map]
