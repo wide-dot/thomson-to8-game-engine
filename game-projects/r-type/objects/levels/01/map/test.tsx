df510ae919a83ffbf6486cd8882d4ff4fafb5d8a
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-02-01 00:50:10 +0100

    updated rtype map names for leanscroll tool

---
+++ game-projects/r-type/objects/levels/01/map/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="test" tilewidth="14" tileheight="14" tilecount="65" columns="1">
+ <image source="0/0.png" trans="ff00ff" width="14" height="910"/>
+</tileset>
