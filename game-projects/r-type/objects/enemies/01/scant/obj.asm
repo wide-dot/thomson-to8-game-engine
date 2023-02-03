3242727f129da636c95cc41e2c26987bf0f2ed43
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-02-03 11:19:43 +0100

    Scant updated AI
    Now keeps its distance (with buffer) from player 1 including tests that it does not come off the screen on the right when flying backward.

--- game-projects/r-type/objects/enemies/01/scant/obj.asm
+++ game-projects/r-type/objects/enemies/01/scant/obj.asm
@@ -11,6 +11,13 @@
 Scant_isshooting        equ ext_variables
 Scant_backfire          equ $80
 Scant_sensitivity       equ 5
+Scant_move_left         equ $-70
+Scant_move_right        equ $70
+Scant_move_up           equ $-50
+Scant_move_down         equ $50
+Scant_keep_distance     equ $30
+Scant_keep_distance_buf equ $10
+
 
 Onject
         lda   routine,u
@@ -37,9 +44,7 @@ Scant_stopshooting
         sta   routine,u
         ldd   #Ani_scant
         std   anim,u   
-        clr   anim_frame,u 
-        ldd   #$-30
-        std   x_vel,u
+        clr   anim_frame,u
 
 Scant_live
         jsr   Scant_whattodo
@@ -65,6 +70,37 @@ Scant_shoot
 
 Scant_whattodo
 
+        ldd   player1+x_pos             ; Let's check is Scant is on the left of player1
+        cmpd  x_pos,u
+        blt   >          
+        ldd   #Scant_move_left          ; Yes Scant is, let's move it to the left to exit
+        jmp   Scant_whattodonext
+!
+        ldd   x_pos,u                   ; Lets check how far Scant is from player 1
+        subd  #Scant_keep_distance
+        cmpd  player1+x_pos
+        blt   >
+        ldd   #Scant_move_left          ; Still far, can move left
+        jmp   Scant_whattodonext
+!
+        addd  #Scant_keep_distance_buf
+        cmpd  player1+x_pos
+        blt   >
+        ldd   #0
+        jmp   Scant_whattodonext
+!
+        ldd   glb_camera_x_pos          ; Scant is too close to player1, let's check if Scant is too far right
+        addd  #130
+        cmpd  x_pos,u
+        bgt   >
+        ldd   #0                        ; Scant is too far on the right, let's cancel its x_vel
+        jmp   Scant_whattodonext        
+
+!
+        ldd   #Scant_move_right         ; Ok, we can move Scant back
+Scant_whattodonext
+        std   x_vel,u
+
         ldd   player1+y_pos
         addd  #Scant_sensitivity
         cmpd  y_pos,u
@@ -75,15 +111,17 @@ Scant_whattodo
         ldd   player1+x_pos
         cmpd  x_pos,u
         blt   >
-        ldd   #00               ; past the r-rtype (on its left), don't shoot
-        std   y_vel,u
+        ldd   #Scant_move_left          ; past the r-rtype (on its left), don't shoot, move it left to exit
+        std   x_vel,u
+        clr   y_vel,u
+        
         rts
 !       
         ldd   glb_camera_x_pos
         addd  #130
         cmpd  x_pos,u
         bgt   >
-        ldd   #00               ; too far on the right of the screen, don't shoot
+        ldd   #00                       ; too far on the right of the screen, don't shoot
         std   y_vel,u
         rts     
 !                               ; time to initiate the shoot
@@ -95,11 +133,11 @@ Scant_whattodo
         inc   routine,u
         rts
 Scant_keepdown
-        ldd   #$30
+        ldd   #Scant_move_down
         std   y_vel,u
         rts
 Scant_keepup
-        ldd   #$-30
+        ldd   #Scant_move_up
         std   y_vel,u
         rts
 Scant_shooting
@@ -136,7 +174,7 @@ Scant_initiateshoot
         ldd   #Ani_scant
         std   anim,u   
         clr   anim_frame,u 
-        ldd   #$-30
+        ldd   #Scant_move_left
         std   x_vel,u
         rts                   
 !
