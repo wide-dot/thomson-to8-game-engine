04280887543971e3c6d6d6e6d4023e7c938242cb
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-02-03 14:53:29 +0100

    Updates on Bink and Bug
    Implemented the shooting strategy in the Bink and Bug enemies.
    
    Updated comments on the objects about the format of their respective subtype
    
    Fixed Issue with horizontal shoot tracking (2 additional overriding the tracking, not sure what they were doing here). Confirmed working now.

--- game-projects/r-type/objects/enemies/01/bug/obj.asm
+++ game-projects/r-type/objects/enemies/01/bug/obj.asm
@@ -4,12 +4,29 @@
 ; input REG : [u] pointer to Object Status Table (OST)
 ; ---------
 ;
+; Bug subtype :
+;       
+;       Bit 0 : 0 => Starts up, 1 => Starts down
+;       Bit 1 : 0 => Doies not shoot, 1 => Shoots
+;       Bit 2 : (Shooting timer) 0 => Shoots early, 1=> Shoot late
+;       Bit 3 : (Unused - Shoot frequency) 0 => Shoots fast, 1=> Shoots slow
+;       Bit 4-5 :
+;             0 -> horizontal
+;             1 -> 30% angle (from horizon)
+;             2 -> 60% angle (from horizon)
+;             3 -> vertical
+;       Bit 6 : 0 => kill tracking OFF, 1 => kill tracking ON
+;
 ; ---------------------------------------------------------------------------
 
         INCLUDE "./engine/macros.asm"
 
-amplitude equ ext_variables ; current amplitude
-amplitude_max equ 60       ; maximum amplitude before direction change
+amplitude         equ ext_variables ; current amplitude
+shoottiming       equ ext_variables+2
+shoottiming_value equ ext_variables+4
+shootnoshoot      equ ext_variables+6
+shootdirection    equ ext_variables+7
+amplitude_max     equ 60       ; maximum amplitude before direction change
 
 Object
         lda   routine,u
@@ -32,19 +49,50 @@ Init
         std   amplitude,u
         ldd   #$-A0
         std   x_vel,u
+        inc   routine,u
+        ldy   #Ani_bug_to_up
+        ldx   #$-A0
+
         lda   subtype,u
-        sta   routine,u
-        cmpa  #1
+        bita  #$01 ; Up or down ?
         bne   >
-        ldd   #Ani_bug_to_up
-        std   anim,u
-        ldd   #$-A0
-        std   y_vel,u
+        ldy   #Ani_bug_to_down
+        ldx   #$A0
+        inc   routine,u
+!
+        stx   y_vel,u
+        sty   anim,u
+
+        bita  #$02 ; Shoot or no shoot
+        bne   >
+        clr   shootnoshoot,u ; No shoot, can skip the end about shooting details
         bra   Object
-!       ldd   #Ani_bug_to_down
-        std   anim,u
-        ldd   #$A0
-        std   y_vel,u
+!
+        ldb   #1
+        stb   shootnoshoot,u
+
+        ldx   #80
+        bita  #$04 ; Starts shooting late or early ?
+        beq   >
+        ldx   #160  ; Starts late
+!
+        stx   shoottiming,u
+
+        ; This is currently unused, hence set at "less frequently" by default (according to findings on R-Type Dimensions)
+        ;ldx   #40  ; Shoots more frequently or less
+        ;bita  #$08
+        ;beq   >
+        ldx   #80  ; Less frequently
+;!
+        stx   shoottiming_value,u
+
+        asra
+        asra
+        asra
+        asra
+        anda  #7
+        sta   shootdirection,u
+
         bra   Object
 
 LiveUp
@@ -75,6 +123,29 @@ LiveDown
         bra   LiveUp
 
 CheckEOL
+        lda   shootnoshoot,u
+        beq   @noshoot
+        ldd   shoottiming,u
+        subd  Vint_Main_runcount_w
+        std   shoottiming,u
+        bpl   @noshoot
+        ldd   shoottiming_value,u
+        std   shoottiming,u
+        jsr   LoadObject_x ; PatapataShoot
+        beq   @noshoot
+        lda   #ObjID_foefire
+        sta   id,x
+        ldd   x_pos,u
+        std   x_pos,x
+        ldd   y_pos,u
+        std   y_pos,x
+        lda   shootdirection,u
+        jsr   ReturnShootDirection_X
+        std   x_vel,x
+        lda   shootdirection,u
+        jsr   ReturnShootDirection_Y
+        std   y_vel,x
+@noshoot
         ldd   x_pos,u
         cmpd  glb_camera_x_pos
         ble   >
