f5085c069765755e65446de52d706c07a8fcda25
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-02-03 22:52:58 +0100

    fixed splah display

--- game-projects/goldorak/objects/splash/splash.asm
+++ game-projects/goldorak/objects/splash/splash.asm
@@ -16,10 +16,13 @@ routines
 Init
         ldb   #LAYER_BACKGROUND
         stb   priority,u
+        lda   render_flags,u
+        ora   #render_overlay_mask
+        sta   render_flags,u
+        ldd   #Img_Background    
+        std   image_set,u
         ldd   #XY_CENTERED_FULL_IMAGE
         std   xy_pixel,u
         inc   routine,u
 Display
-        ldd   #Img_Background    
-        std   image_set,u
         jmp   DisplaySprite
