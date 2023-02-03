2292a25467fcf5348dfa3196075883ef1e5197b8
François-Xavier Robin <fxrobin@gmail.com>
2023-02-03 17:27:09 +0100

    tentatives de faire afficher une image ...

--- game-projects/goldorak/objects/splash/splash.asm
+++ game-projects/goldorak/objects/splash/splash.asm
@@ -1,6 +1,6 @@
 
 XY_CENTERED_FULL_IMAGE EQU $807F 
-LAYER_BACKGROUND EQU #7
+LAYER_BACKGROUND EQU 7
 
         INCLUDE "./engine/macros.asm"
 
@@ -14,12 +14,9 @@ routines
 
 
 Init
-        ldb   LAYER_BACKGROUND                       ; ici je suis perdu 
+        ldb   #LAYER_BACKGROUND
         stb   priority,u
-        lda   render_flags,u
-        ora   #render_overlay_mask
-        sta   render_flags,u
-        ldd   XY_CENTERED_FULL_IMAGE
+        ldd   #XY_CENTERED_FULL_IMAGE
         std   xy_pixel,u
         inc   routine,u
 Display
