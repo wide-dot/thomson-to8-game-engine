baad0be0ffcbc998d52c88249bac87996776a307
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-02-05 23:51:12 +0100

    Add Collision to engine

--- game-projects/unit-test/objects/box/cbox.asm
+++ game-projects/unit-test/objects/box/cbox.asm
@@ -1,4 +1,7 @@
         INCLUDE "./engine/macros.asm" 
+        INCLUDE "./engine/collision/struct_AABB.equ"
+
+AABB_0  equ ext_variables ; AABB struct (9 bytes)
 
 Obj01
         lda   render_flags,u
@@ -11,36 +14,37 @@ Obj01
         jmp   [a,x]
 
 Obj01_Index
-        fdb   Obj01_Init
-        fdb   Obj01_Main_p1
-        fdb   Obj01_Main_p2
+        fdb   Obj01_Init1
+        fdb   Obj01_Init2
+        fdb   Obj01_Main1
+        fdb   Obj01_Main2
 
-Obj01_Init
+Obj01_Init1
         ldd   #Img_Box24x20
         std   image_set,u
         lda   #2
         sta   priority,u
         lda   #render_playfieldcoord_mask
         sta   render_flags,u
+        inc   routine,u
+        jmp   DisplaySprite
+
+Obj01_Init2
+        leax  AABB_0,u
         lda   subtype,u
         bne   >
+        jsr   AddPlayerAABB
+        inc   routine,u
+        bra   @skip
+!       jsr   AddAiAABB
+@skip
         lda   #1
-        sta   AABB_a.p
+        sta   AABB.p,x
         _ldd  24/2,20/2
-        std   AABB_a.rx
-        lda   #1
-        sta   routine,u
-        rts
-!       
-        lda   #1
-        sta   AABB_b.p
-        _ldd  24/2,20/2
-        std   AABB_b.rx
-        lda   #2
-        sta   routine,u
-        rts
+        std   AABB.rx,x
+        inc   routine,u
 
-Obj01_Main_p1
+Obj01_Main1
         lda   Dpad_Held
         ldb   Fire_Press
 @TestLeft
@@ -70,58 +74,17 @@ Obj01_Main_p1
         leax  1,x
         stx   y_pos,u
 @TestBtn
-        bitb  #c1_button_A_mask
-        beq   >
-!       
+        ;bitb  #c1_button_A_mask
+        ;beq   >
+Obj01_Main2
+        leax  AABB_0,u
         ldd   #Img_Box24x20
-        tst   AABB_a.p
+        tst   AABB.p,x
         bne   >
-        inc   AABB_a.p
+        inc   AABB.p,x
         ldd   #Img_Box24x20s
 !       std   image_set,u
         ldd   xy_pixel,u
-        std   AABB_a.cx
+        std   AABB.cx,x
         jmp   DisplaySprite
-
-Obj01_Main_p2
-        lda   Dpad_Held
-        ldb   Fire_Press
-@TestLeft
-        bita  #c2_button_left_mask
-        beq   @TestRight   
-        ldx   x_pos,u
-        leax  -1,x
-        stx   x_pos,u
-        bra   @TestUp
-@TestRight        
-        bita  #c2_button_right_mask
-        beq   @TestUp   
-        ldx   x_pos,u
-        leax  1,x
-        stx   x_pos,u
-@TestUp
-        bita  #c2_button_up_mask
-        beq   @TestDown   
-        ldx   y_pos,u
-        leax  -1,x
-        stx   y_pos,u
-        bra   @TestBtn
-@TestDown
-        bita  #c2_button_down_mask
-        beq   @TestBtn   
-        ldx   y_pos,u
-        leax  1,x
-        stx   y_pos,u
-@TestBtn
-        bitb  #c2_button_A_mask
-        beq   >
-!       
-        ldd   #Img_Box24x20
-        tst   AABB_b.p
-        bne   >
-        inc   AABB_b.p
-        ldd   #Img_Box24x20s
-!       std   image_set,u
-        ldd   xy_pixel,u
-        std   AABB_b.cx
-        jmp   DisplaySprite
+   
