fbc1e273c49d206dfc7c2bccf9e0edeafdd10e91
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-02-27 14:29:48 +0100

    Add beam bar to hud (rtype)

--- game-projects/r-type/objects/levels/hud/hud.asm
+++ game-projects/r-type/objects/levels/hud/hud.asm
@@ -1,8 +1,122 @@
+; *****************************************************************************
+; Render HUD on screen
+; --------------------
+;
+; - nb of lives
+; - beam indicator
+; - score
+;
+; *****************************************************************************
+
+_beam_seg_extB MACRO                    ; outer lines of 12px
+	PSHU A,X
+
+	LEAU 2*40+3,U
+	PSHU A,X
+ ENDM
+
+_beam_seg_extA MACRO                    ; outer lines of 12px
+        LEAU $2000-2*40+3+1,U
+	PSHU A,X
+
+	LEAU 2*40+3,U
+	PSHU A,X
+ ENDM
+
+_beam_seg_intA MACRO                    ; inner line of 12px
+	LEAU -37,U
+	PSHU A,X
+ ENDM
+
+_beam_seg_intB MACRO                    ; inner line of 12px
+        LEAU $-2000+3-1,U
+	PSHU A,X
+
+        LEAU -37+3,U                    ; move to next segment position
+ ENDM
+
+; replace this line with an INCLUDE of player1 equates
+beam_value       equ ext_variables
+
+beam_m_start equ $BE87                 ; beam render starting point
+beam_m_size  equ 3                     ; number of byte for a segment
+
+        ; display beam in 5 segments
+        ldu   #beam_m_start
+
+        ldb   player1+beam_value
+@loop_full_beam
+        tstb
+        lbeq  @loop_black
+        subb  #12
+        bmi   @do_partial_beam
+	Lda   #$55
+	Ldx   #$5555
+        _beam_seg_extB
+        _beam_seg_extA
+	Lda   #$66
+	Ldx   #$6666
+        _beam_seg_intA
+        _beam_seg_intB
+        cmpu  #beam_m_start+beam_m_size*5
+        lbeq  @beam_end
+        bra   @loop_full_beam
+;
+@do_partial_beam
+        ldy   #Beam_mask
+        lda   #6
+        negb
+        mul
+        leay  d,y
+        ldd   1,y
+        anda  #$55
+        andb  #$55
+        tfr   d,x
+        lda   ,y
+        anda  #$55
+        _beam_seg_extB
+        ldd   4,y
+        anda  #$55
+        andb  #$55
+        tfr   d,x
+        lda   3,y
+        anda  #$55
+        _beam_seg_extA
+        ldd   4,y
+        anda  #$66
+        andb  #$66
+        tfr   d,x
+        lda   3,y
+        anda  #$66
+        _beam_seg_intA
+        ldd   1,y
+        anda  #$66
+        andb  #$66
+        tfr   d,x
+        lda   ,y
+        anda  #$66
+        _beam_seg_intB
+        cmpu  #beam_m_start+beam_m_size*5
+        beq   @beam_end
+;
+@loop_black
+        lda   #$00                     ; complete the bar with black segments
+	Ldx   #$0000
+        _beam_seg_extB
+        _beam_seg_extA
+        _beam_seg_intA
+        _beam_seg_intB
+        cmpu  #beam_m_start+beam_m_size*5
+        bne   @loop_black
+@beam_end
+
+        ; display lives
         ldb   #1
         ldx   lives
         ldu   #$DEA4
         jsr   DisplayDigit
 
+        ; display score
         ldx   score
         beq   >
         ldb   #4                       ; nb of char
@@ -18,6 +132,7 @@
         ldu   #$DEC0
         jmp   DisplayDigit
 
+; ----------------------------------------------------
 ; Display a n digit number on screen (mode 160x200x16)
 ;
 ; INPUT
@@ -375,3 +490,15 @@ DRAW_Img_hud_9
 	STA -120,U
         puls  u,pc
 
+Beam_mask
+        fcb $ff,$ff,$ff,$ff,$ff,$f0 ; (11) RAMB: A, XH, XL RAMA: A, XH, XL
+        fcb $ff,$ff,$ff,$ff,$ff,$00 : (10)
+        fcb $ff,$ff,$f0,$ff,$ff,$00
+        fcb $ff,$ff,$00,$ff,$ff,$00
+        fcb $ff,$ff,$00,$ff,$f0,$00
+        fcb $ff,$ff,$00,$ff,$00,$00
+        fcb $ff,$f0,$00,$ff,$00,$00
+        fcb $ff,$00,$00,$ff,$00,$00
+        fcb $ff,$00,$00,$f0,$00,$00
+        fcb $ff,$00,$00,$00,$00,$00
+        fcb $f0,$00,$00,$00,$00,$00 ; (01)
