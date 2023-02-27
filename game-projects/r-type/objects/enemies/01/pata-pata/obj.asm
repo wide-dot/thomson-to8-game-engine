3b3a7bd4d3edfb91ac5e13970ed99e282b839a5b
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-02-27 08:58:23 +0100

    Enemies properties file, and implementation into patapara

--- game-projects/r-type/objects/enemies/01/pata-pata/obj.asm
+++ game-projects/r-type/objects/enemies/01/pata-pata/obj.asm
@@ -22,6 +22,7 @@
         INCLUDE "./engine/macros.asm"
         INCLUDE "./engine/collision/macros.asm"
         INCLUDE "./engine/collision/struct_AABB.equ"
+        INCLUDE "./game-projects/r-type/objects/enemies_properties.asm"
 
 AABB_0            equ ext_variables   ; AABB struct (9 bytes)
 amplitude         equ ext_variables+9 ; current amplitude
@@ -61,9 +62,9 @@ Init
         _Collision_AddAABB AABB_0,AABB_list_ennemy
         
         leax  AABB_0,u
-        lda   #1                       ; set damage potential for this hitbox
+        lda   #patapata_hitdamage                       ; set damage potential for this hitbox
         sta   AABB.p,x
-        _ldd  4,8                      ; set hitbox xy radius
+        _ldd  patapata_hitbox_x,patapata_hitbox_y                      ; set hitbox xy radius
         std   AABB.rx,x
 
         ldx   #$-A0
@@ -167,7 +168,7 @@ CheckEOL
         jmp   DisplaySprite
 @destroy 
         ldd   score
-        addd  #2
+        addd  #patapata_score
         std   score
         jsr   LoadObject_x
         beq   @delete
