497e5d1d72aca69d96d76de643c8a9bac38e57dd
François-Xavier Robin <fxrobin@gmail.com>
2023-03-10 17:46:11 +0100

    optimisation de cyles

--- engine/object-management/RunObjects.asm
+++ engine/object-management/RunObjects.asm
@@ -88,7 +88,7 @@ LoadObject_x
         rts                            ; return z=1 when not found
 @link
         pshs  u                        ; let's save U
-        tfr x,u                        ; X contains de address of the stack, so let's transfer it to U
+        leau ,x                        ; X contains de address of the stack, so let's transfer it to U
         pulu X                         ; get a the free slot from the slot stack, X points this free slot
         stu STACK_POINTER              ; updating the slot stack pointer
         ldu   object_list_last
