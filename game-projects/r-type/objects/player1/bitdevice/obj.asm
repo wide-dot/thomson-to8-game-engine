942ef86d622d585822ccfd6b51a26806775e02b2
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-03-15 10:03:35 +0100

    Bit device - beginning implementation

--- game-projects/r-type/objects/player1/bitdevice/obj.asm
+++ game-projects/r-type/objects/player1/bitdevice/obj.asm
@@ -7,19 +7,25 @@
 ; ---------------------------------------------------------------------------
 
         INCLUDE "./engine/macros.asm"
+        INCLUDE "./engine/collision/macros.asm"
+        INCLUDE "./engine/collision/struct_AABB.equ"
+        INCLUDE "./objects/player1/player1.equ"
+AABB_0            equ ext_variables   ; AABB struct (9 bytes)
 
-Onject
+Object
         lda   routine,u
         asla
         ldx   #Routines
         jmp   [a,x]
 
 Routines
-        fdb   Init
+        fdb   InitOptionBox
+        fdb   LiveOptionBox
+        fdb   AlreadyDeletedOptionBox
         fdb   Live
 
-Init
-        ldd   #Ani_weapon_5
+InitOptionBox
+        ldd   #Ani_bitdevice
         std   anim,u
         ldb   #6
         stb   priority,u
@@ -28,12 +34,54 @@ Init
         sta   render_flags,u
         inc   routine,u
 
-Live
+        _Collision_AddAABB AABB_0,AABB_list_bonus
+        
+        leax  AABB_0,u
+        lda   #1                        ; set damage potential for this hitbox
+        sta   AABB.p,x
+        _ldd  4,7                       ; set hitbox xy radius
+        std   AABB.rx,x
+
+LiveOptionBox
         ldd   x_pos,u
-        addd  1
-        std   x_pos,u
         cmpd  glb_camera_x_pos
         ble   >
+        leax  AABB_0,u
+        lda   AABB.p,x
+        beq   Init                     ; was touched  
+        ldd   x_pos,u
+        subd  glb_camera_x_pos
+        stb   AABB.cx,x
+        ldb   y_pos+1,u
+        stb   AABB.cy,x
         jsr   AnimateSpriteSync
         jmp   DisplaySprite
-!       jmp   DeleteObject
+!       
+        _Collision_RemoveAABB AABB_0,AABB_list_bonus
+        inc   routine,u
+        jmp   DeleteObject
+AlreadyDeletedOptionBox
+        rts
+Init
+        _Collision_RemoveAABB AABB_0,AABB_list_bonus
+        _Collision_AddAABB AABB_0,AABB_list_friend
+        
+        leax  AABB_0,u
+        lda   #255                      ; set damage potential for this hitbox
+        sta   AABB.p,x
+        _ldd  3,6                       ; set hitbox xy radius
+        std   AABB.rx,x
+        ldd   y_pos,u
+        stb   AABB.cy,x
+
+        lda   #3
+        sta   routine,u
+Live
+        ldd   player1+x_pos
+        std   x_pos,u
+        ldd   player1+y_pos
+        subd  #16
+        std   y_pos,u
+        jsr   AnimateSpriteSync
+        jmp   DisplaySprite
+
