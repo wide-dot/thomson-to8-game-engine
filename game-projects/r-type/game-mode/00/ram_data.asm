beab729956d729163a828bbee042e4c696a194ac
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-03-19 11:47:20 +0100

    Trying title screen

---
+++ game-projects/r-type/game-mode/00/ram_data.asm
@@ -0,0 +1,16 @@
+
+* ===========================================================================
+* Object Constants
+* ===========================================================================
+nb_dynamic_objects           equ 64
+nb_graphical_objects         equ 64 * max 64 total
+ext_variables_size           equ 20 ; ext_variables_size is for dynamic objects
+
+* ===========================================================================
+* Object Status Table - OST
+* ===========================================================================
+palettefade                  fcb   ObjID_fade
+                             fill  0,10-1
+
+Dynamic_Object_RAM           fill  0,(nb_dynamic_objects)*10
+Dynamic_Object_RAM_End
