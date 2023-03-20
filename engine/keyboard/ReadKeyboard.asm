bd293c3b23c96ab99b1e665d7043c04fb2a14f5b
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-03-20 12:45:15 +0100

    Add Keybord routine to engine

---
+++ engine/keyboard/ReadKeyboard.asm
@@ -0,0 +1,22 @@
+********************************************************************************
+* Get Keyboard codes
+*
+********************************************************************************
+
+Key_Press                   fcb   $00
+Key_Held                    fcb   $00 
+
+ReadKeyboard
+        clrb
+        stb   Key_Press
+        jsr   KTST       ; was a key pressed ?
+        bcc   @clearHeld ; no exit
+        jsr   GETC       ; read new key code in b
+        cmpb  Key_Held
+        beq   @rts       ; return if key is already held, Press was cleared, but not Held
+        stb   Key_Held   ; new key code was read
+        stb   Key_Press  ; store new key for one main loop
+@rts    rts
+@clearHeld
+        stb   Key_Held   ; no more key pressed
+        rts
