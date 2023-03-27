e01afd19906f19dade4acad63bd7f40052a6b8f3
François-Xavier Robin <fxrobin@gmail.com>
2023-03-27 16:07:59 +0200

    glitch scrolling

--- game-projects/goldorak/game-mode/gamescreen/gamescreenRamData.asm
+++ game-projects/goldorak/game-mode/gamescreen/gamescreenRamData.asm
@@ -15,12 +15,37 @@ MainCharacter                 equ dp
 Dynamic_Object_RAM            fill  0,(nb_dynamic_objects)*object_size
 Dynamic_Object_RAM_End
 
+
+Tile_Data_RAM_A_Start
+   FDB $1111,$1111,$1111,$1111  ; 32 px x 8 px 
+   FDB $2222,$2222,$2222,$2222  ; 
+   FDB $3333,$3333,$3333,$3333  ; 
+   FDB $4444,$4444,$4444,$4444  ; 
+   FDB $5555,$5555,$5555,$5555  ;
+   FDB $6666,$6666,$6666,$6666  ;
+   FDB $7777,$7777,$7777,$7777  ;
+   FDB $8888,$8888,$8888,$8888  ;
+Tile_Data_RAM_A_End
+
+Tile_Data_RAM_B_Start
+   FDB $1111,$1111,$1111,$1111  ; 
+   FDB $2222,$2222,$2222,$2222  ; 
+   FDB $3333,$3333,$3333,$3333  ; 
+   FDB $4444,$4444,$4444,$4444  ; 
+   FDB $5555,$5555,$5555,$5555  ;
+   FDB $6666,$6666,$6666,$6666  ;
+   FDB $7777,$7777,$7777,$7777  ;
+   FDB $8888,$8888,$8888,$8888  ;
+Tile_Data_RAM_B_End
+
+
 Obj_PaletteFade FDB $0000
 
-RAM_A_COCKPIT
+RAM_A_COCKPIT_START
         INCLUDEBIN "./game-mode/gamescreen/image/goldorak-cockpit.1.0.bin"
-RAM_B_COCKPIT
+RAM_A_COCKPIT_END
+RAM_B_COCKPIT_START
         INCLUDEBIN "./game-mode/gamescreen/image/goldorak-cockpit.0.0.bin"
-
+RAM_B_COCKPIT_END
 
 
