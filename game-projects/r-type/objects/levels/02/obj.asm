5a4b25701a95db3cde15e547cd4ee5cb71643d00
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-05 09:39:55 +0200

    Preparing fondations for level 02

--- game-projects/r-type/objects/levels/02/obj.asm
+++ game-projects/r-type/objects/levels/02/obj.asm
@@ -1,31 +1,41 @@
-Level
-        lda   routine,u
-        asla
-        ldx   #Level_Routines
-        jmp   [a,x]
+        INCLUDE "./engine/macros.asm"
 
-Level_Routines
-        fdb   Level_Init_0
-        fdb   Level_Init_1
-        fdb   Level_End
+LevelInit
 
-Level_Init_0
-        ldd   #Img_lvl02_0
-        std   image_set,u
-	ldd   #$806B
-        std   xy_pixel,u
-	ldb   #7
-        stb   priority,u
-        lda   render_flags,u
-        ora   #render_overlay_mask
-        sta   render_flags,u
-	inc   routine,u
-        jmp   DisplaySprite	
+        ; register map location to main
+        ldd   #Tls_lvl02
+        std   scroll_map_even
+        ldd   #Tls_lvl02_s
+        std   scroll_map_odd
+        _GetCartPageA
+        sta   scroll_map_page_even
+        sta   scroll_map_page_odd
 
-Level_Init_1
-	inc   routine,u
-        jmp   DisplaySprite
+        ; set scroll parameters
+        _ldd  12,15
+        sta   scroll_vp_h_tiles
+        stb   scroll_vp_v_tiles
+        _ldd  8,11
+        sta   scroll_vp_x_pos
+        stb   scroll_vp_y_pos
+        _ldd  12,12
+        sta   scroll_tile_width
+        stb   scroll_tile_height
+        ldd   #$0030 ; 8.8 scroll speed, r-type arcade scroll is 0,5px / frame so here (256*0.5)/(384/144)
+        std   scroll_vel
 
-Level_End
-        jmp   DeleteObject
+        rts
 
+Tls_lvl02
+        INCLUDEGEN Tls_lvl02 buffer
+        ; pre-rendered tilemap buffer
+        ; page    (byte) page number of compilated tile routine
+        ; address (word) absolute address of compilated tile routine
+        ; [repeated for each tile in the map]
+
+Tls_lvl02_s
+        INCLUDEGEN Tls_lvl02_s buffer
+        ; pre-rendered tilemap buffer
+        ; page    (byte) page number of compilated tile routine
+        ; address (word) absolute address of compilated tile routine
+        ; [repeated for each tile in the map]
