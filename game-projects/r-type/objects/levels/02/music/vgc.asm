05f5fddbb99bb80be41c769271044dd1385979f0
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-09 12:09:37 +0200

    Level 2 musique integration and some cleanup

--- game-projects/r-type/objects/levels/02/music/vgc.asm
+++ game-projects/r-type/objects/levels/02/music/vgc.asm
@@ -2,11 +2,7 @@
         INCLUDE "./engine/objects/sound/vgc/vgc.asm"
 
 Snd_index
-        fdb   Snd_01_intro_sn
-        fdb   Snd_01_theme_sn
+        fdb   Snd_02_theme_sn
 
-Snd_01_intro_sn
-        INCLUDEBIN "./objects/levels/01/music/adnz/vgc/intro.vgc
-
-Snd_01_theme_sn
-        INCLUDEBIN "./objects/levels/01/music/adnz/vgc/theme.vgc
+Snd_02_theme_sn
+        INCLUDEBIN "./objects/levels/02/music/adnz/vgc/theme.vgc
--- game-projects/r-type/objects/levels/02/music/ymm.asm
+++ game-projects/r-type/objects/levels/02/music/ymm.asm
@@ -2,11 +2,7 @@
         INCLUDE "./engine/objects/sound/ymm/ymm.asm"
 
 Snd_index
-        fdb   Snd_01_intro_ym
-        fdb   Snd_01_theme_ym
+        fdb   Snd_02_theme_ym
 
-Snd_01_intro_ym
-        INCLUDEBIN "./objects/levels/01/music/adnz/ymm/intro.ymm.zx0
-
-Snd_01_theme_ym
-        INCLUDEBIN "./objects/levels/01/music/adnz/ymm/theme.ymm.zx0
+Snd_02_theme_ym
+        INCLUDEBIN "./objects/levels/02/music/adnz/ymm/theme.ymm.zx0
