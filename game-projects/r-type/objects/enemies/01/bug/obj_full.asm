cde752cc03fb2ff19544cbb00cb1da7801961ff4
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-09 11:41:45 +0200

    Reorg of the bug object to allow sprite variants (so unused images aren't loaded for a particular level)

---
+++ game-projects/r-type/objects/enemies/01/bug/obj_full.asm
@@ -0,0 +1,27 @@
+; ---------------------------------------------------------------------------
+; Object
+;
+; input REG : [u] pointer to Object Status Table (OST)
+; ---------
+;
+; ---------------------------------------------------------------------------
+
+        INCLUDE "./objects/enemies/01/bug/obj_main.asm"
+ImageIndex
+        fdb   Img_bug_0
+        fdb   Img_bug_1
+        fdb   Img_bug_2
+        fdb   Img_bug_3
+        fdb   Img_bug_4
+        fdb   Img_bug_5
+        fdb   Img_bug_6
+        fdb   Img_bug_7
+        fdb   Img_bug_8
+        fdb   Img_bug_9
+        fdb   Img_bug_10
+        fdb   Img_bug_11
+        fdb   Img_bug_12
+        fdb   Img_bug_13
+        fdb   Img_bug_14
+        fdb   Img_bug_15
+ 
