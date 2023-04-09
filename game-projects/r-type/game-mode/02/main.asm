fe36cd6de2f157758ee7d053272e5d0df4b603f6
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-09 12:09:37 +0200

    Level 2 musique integration and some cleanup

--- game-projects/r-type/game-mode/02/main.asm
+++ game-projects/r-type/game-mode/02/main.asm
@@ -66,9 +66,9 @@ CHECKPOINT_01_wave equ (56-2)*12*2
 
 ; play music
         _MountObject ObjID_ymm02
-        _MusicInit_objymm #0,#MUSIC_NO_LOOP,#MusicCallbackYM  ; initialize the YM2413 player 
+        _MusicInit_objymm #0,#MUSIC_LOOP,#0
         _MountObject ObjID_vgc02
-        _MusicInit_objvgc #0,#MUSIC_NO_LOOP,#MusicCallbackSN ; initialize the SN76489 vgm player with a vgc data stream
+        _MusicInit_objvgc #0,#MUSIC_LOOP,#0
 
 ; init user irq
         jsr   IrqInit
@@ -123,16 +123,6 @@ UserIRQ
         _MusicFrame_objymm
         _MountObject ObjID_vgc02
         _MusicFrame_objvgc
-        rts
-
-MusicCallbackYM
-        _MountObject ObjID_ymm02
-        _MusicInit_objymm #1,#MUSIC_LOOP,#0
-        rts
-
-MusicCallbackSN
-        _MountObject ObjID_vgc02
-        _MusicInit_objvgc #1,#MUSIC_LOOP,#0
         rts
 
 * ---------------------------------------------------------------------------
