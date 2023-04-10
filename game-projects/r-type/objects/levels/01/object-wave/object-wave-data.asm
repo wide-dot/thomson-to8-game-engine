c442c6ea8ce34a89b7dbc8c050d1e1b0a503b0af
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-10 13:14:14 +0200

    Updated blaster subtype to match arcade version. Updated wave with arcade settings for blaster

--- game-projects/r-type/objects/levels/01/object-wave/object-wave-data.asm
+++ game-projects/r-type/objects/levels/01/object-wave/object-wave-data.asm
@@ -42,53 +42,53 @@
 	fcb   $08,$8C,ObjID_bug,$01,$34
 	fcb   $09,$4C,ObjID_bug,$00,$36
 
-	fcb   $0B,$86,ObjID_blaster,41,230
-	fcb   $0B,$86,ObjID_blaster,159,241
-	fcb   $0B,$C6,ObjID_blaster,41,250
-	fcb   $0B,$C6,ObjID_blaster,159,221
-	fcb   $0C,$06,ObjID_blaster,53,230
-	fcb   $0C,$06,ObjID_blaster,147,223
-	fcb   $0C,$46,ObjID_blaster,53,230
-	fcb   $0C,$46,ObjID_blaster,147,223
+        fcb   $0B,$90,ObjID_blaster,$00,$11
+        fcb   $0B,$90,ObjID_blaster,$00,$04
+        fcb   $0B,$D0,ObjID_blaster,$00,$01
+        fcb   $0B,$D0,ObjID_blaster,$00,$04
+        fcb   $0C,$10,ObjID_blaster,$00,$12
+        fcb   $0C,$10,ObjID_blaster,$00,$03
+        fcb   $0C,$50,ObjID_blaster,$00,$02
+        fcb   $0C,$50,ObjID_blaster,$00,$03
 
 	fcb   $0C,$FE,ObjID_fadetotunnel,0,0
 
 	fcb   $0D,$88,ObjID_pow,$00,$35
-	fcb   $0E,$06,ObjID_blaster,29,220
+        fcb   $0E,$10,ObjID_blaster,$00,$00
 	fcb   $0E,$2C,ObjID_pow,$00,$04
-	fcb   $0E,$46,ObjID_blaster,29,250
+        fcb   $0E,$50,ObjID_blaster,$00,$00
 	fcb   $0E,$50,ObjID_bug,$01,$22
 	fcb   $0E,$80,ObjID_shell,$00,$00
 
-	fcb   $11,$06,ObjID_blaster,41,230
-	fcb   $11,$46,ObjID_blaster,41,240
-	fcb   $11,$86,ObjID_blaster,29,250
-	fcb   $11,$C6,ObjID_blaster,29,220
+        fcb   $11,$10,ObjID_blaster,$00,$01
+        fcb   $11,$50,ObjID_blaster,$00,$01
+        fcb   $11,$90,ObjID_blaster,$00,$10
+        fcb   $11,$D0,ObjID_blaster,$00,$00
 	fcb   $11,$DA,ObjID_patapata,$00,$2A
 	fcb   $11,$EC,ObjID_patapata,$00,$29
-	fcb   $12,$06,ObjID_blaster,29,230
 	fcb   $12,$0A,ObjID_patapata,$00,$3A
-	fcb   $12,$46,ObjID_blaster,29,250
+        fcb   $12,$10,ObjID_blaster,$00,$10
+        fcb   $12,$50,ObjID_blaster,$00,$30
 	fcb   $12,$60,ObjID_patapata,$00,$24
 	fcb   $12,$82,ObjID_pow,$00,$57
-	fcb   $12,$86,ObjID_blaster,29,240
+        fcb   $12,$90,ObjID_blaster,$00,$00
 	fcb   $12,$C4,ObjID_patapata,$00,$08
-	fcb   $12,$C6,ObjID_blaster,29,250
+        fcb   $12,$D0,ObjID_blaster,$00,$20
 	fcb   $13,$1E,ObjID_pow,$00,$47
-	fcb   $13,$86,ObjID_blaster,53,230
-	fcb   $13,$86,ObjID_blaster,147,223
-	fcb   $13,$C6,ObjID_blaster,53,230
-	fcb   $13,$C6,ObjID_blaster,147,223
+        fcb   $13,$90,ObjID_blaster,$00,$02
+        fcb   $13,$90,ObjID_blaster,$00,$03
 	fcb   $13,$C8,ObjID_bug,$02,$22
+        fcb   $13,$D0,ObjID_blaster,$00,$02
+        fcb   $13,$D0,ObjID_blaster,$00,$13
 	fcb   $14,$B8,ObjID_pow,$00,$75
 
 
-	fcb   $16,$06,ObjID_blaster,29,230
-	fcb   $16,$46,ObjID_blaster,29,240
-	fcb   $16,$86,ObjID_blaster,29,250
-	fcb   $16,$c6,ObjID_blaster,29,230
-	fcb   $17,$06,ObjID_blaster,41,240
-	fcb   $17,$46,ObjID_blaster,41,230
+        fcb   $16,$10,ObjID_blaster,$00,$00
+        fcb   $16,$50,ObjID_blaster,$00,$00
+        fcb   $16,$90,ObjID_blaster,$00,$20
+        fcb   $16,$D0,ObjID_blaster,$00,$00
+        fcb   $17,$10,ObjID_blaster,$00,$01
+        fcb   $17,$50,ObjID_blaster,$00,$11
 	fcb   $18,$22,ObjID_pow,$00,$04
 
 	fcb   $19,$80,ObjID_fadetotunnel,$01,$00
