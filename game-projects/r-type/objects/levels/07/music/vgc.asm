f9e9d589051303807d0af55c435a2d19cd0cff6e
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-12 15:22:27 +0200

    Level 7

---
+++ game-projects/r-type/objects/levels/07/music/vgc.asm
@@ -0,0 +1,16 @@
+
+        INCLUDE "./engine/objects/sound/vgc/vgc.asm"
+
+Snd_index
+        fdb   Snd_07_theme_sn
+        fdb   Snd_boss_intro_sn
+        fdb   Snd_boss_theme_sn
+
+Snd_07_theme_sn
+        INCLUDEBIN "./objects/levels/07/music/adnz/vgc/theme.vgc
+
+Snd_boss_intro_sn
+        INCLUDEBIN "./objects/levels/bossmusic/music/vgc/bossmusic_intro.vgc
+
+Snd_boss_theme_sn
+        INCLUDEBIN "./objects/levels/bossmusic/music/vgc/bossmusic_theme.vgc
