99051c70465ae7031423d0f898b5a2ddafdd3ba1
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-27 11:45:44 +0200

    Bink - Terrain collision implementation (WIP)
    Terrain collision right/left and walk right/left implementations (WIP)

--- game-projects/r-type/objects/levels/01/object-wave/object-wave-data.asm
+++ game-projects/r-type/objects/levels/01/object-wave/object-wave-data.asm
@@ -1,7 +1,20 @@
-	fcb   $01,$40,ObjID_pow,$00,$47
-	fcb   $01,$80,ObjID_pow,$00,$47
-	fcb   $01,$c0,ObjID_pow,$00,$47
-
+;$00,$00,ObjID_0,$00,$00
+;$00,$08,ObjID_33,$00,$00
+;$00,$08,ObjID_32,$00,$04
+;$00,$08,ObjID_32,$00,$05
+;$00,$08,ObjID_32,$00,$06
+;$00,$08,ObjID_32,$00,$07
+;$00,$08,ObjID_32,$00,$08
+;$00,$08,ObjID_32,$00,$09
+;$01,$80,ObjID_0,$00,$01
+;$01,$80,ObjID_1,$00,$00
+;$01,$8C,ObjID_33,$00,$02
+;$01,$8C,ObjID_32,$00,$0A
+;$01,$8C,ObjID_32,$00,$0B
+;$01,$8C,ObjID_32,$00,$0C
+;$01,$8C,ObjID_32,$00,$0D
+;$01,$8C,ObjID_32,$00,$0E
+;$01,$8C,ObjID_32,$00,$0F
 	fcb   $01,$F8,ObjID_patapata,$00,$04
 	fcb   $02,$06,ObjID_patapata,$00,$03
 	fcb   $02,$1A,ObjID_patapata,$00,$05
@@ -11,6 +24,7 @@
 	fcb   $02,$7E,ObjID_patapata,$00,$2A
 	fcb   $02,$90,ObjID_patapata,$00,$09
 	fcb   $02,$B6,ObjID_bug,$01,$46
+	fcb   $02,$EC,ObjID_bink,$00,$88
 	fcb   $03,$40,ObjID_bug,$01,$37
 	fcb   $03,$DC,ObjID_patapata,$00,$0A
 	fcb   $03,$E6,ObjID_patapata,$00,$09
@@ -35,64 +49,113 @@
 	fcb   $05,$88,ObjID_pow,$00,$06
 	fcb   $05,$88,ObjID_patapata,$00,$07
 	fcb   $05,$C6,ObjID_patapata,$00,$06
+	fcb   $06,$0C,ObjID_bink,$00,$88
 	fcb   $06,$2C,ObjID_patapata,$00,$04
 	fcb   $06,$3E,ObjID_patapata,$00,$0A
 	fcb   $06,$98,ObjID_patapata,$00,$04
 	fcb   $06,$AC,ObjID_patapata,$00,$05
+;$06,$AC,ObjID_24,$00,$03
+;$06,$AC,ObjID_24,$00,$04
+;$06,$AC,ObjID_24,$00,$05
+;$06,$AC,ObjID_24,$00,$06
+;$06,$AC,ObjID_24,$00,$07
+;$06,$AC,ObjID_24,$00,$08
+	fcb   $06,$C0,ObjID_bink,$00,$08
+;	fcb   $06,$FC,ObjID_scant,$00,$05
+	fcb   $07,$24,ObjID_bink,$00,$08
+;$07,$C4,ObjID_21,$00,$08
+	fcb   $07,$D0,ObjID_bink,$00,$08
+	fcb   $08,$0E,ObjID_bink,$01,$0C
+	fcb   $08,$6C,ObjID_bink,$00,$17
 	fcb   $08,$8C,ObjID_bug,$01,$34
+;$09,$00,ObjID_0,$00,$02
 	fcb   $09,$4C,ObjID_bug,$00,$36
-
-        fcb   $0B,$90,ObjID_blaster,$00,$11
-        fcb   $0B,$90,ObjID_blaster,$00,$04
-        fcb   $0B,$D0,ObjID_blaster,$00,$01
-        fcb   $0B,$D0,ObjID_blaster,$00,$04
-        fcb   $0C,$10,ObjID_blaster,$00,$12
-        fcb   $0C,$10,ObjID_blaster,$00,$03
-        fcb   $0C,$50,ObjID_blaster,$00,$02
-        fcb   $0C,$50,ObjID_blaster,$00,$03
-
-	fcb   $0C,$FE,ObjID_fadetotunnel,0,0
-
+	fcb   $09,$8C,ObjID_bink,$01,$1C
+	fcb   $09,$8C,ObjID_bink,$00,$12
+;$0A,$12,ObjID_6,$00,$06
+;$0A,$44,ObjID_6,$00,$24
+;$0A,$92,ObjID_6,$00,$07
+;$0A,$A4,ObjID_6,$00,$03
+;$0A,$B4,ObjID_6,$00,$05
+;$0A,$CC,ObjID_6,$00,$06
+;$0A,$E4,ObjID_6,$00,$03
+;$0A,$FE,ObjID_6,$00,$06
+;$0B,$08,ObjID_6,$00,$05
+;$0B,$08,ObjID_6,$00,$07
+;$0B,$1C,ObjID_6,$00,$13
+;$0B,$26,ObjID_6,$00,$04
+;$0B,$3A,ObjID_6,$00,$07
+;$0B,$58,ObjID_6,$00,$05
+;$0B,$6C,ObjID_6,$00,$03
+;$0B,$80,ObjID_6,$00,$06
+	fcb   $0B,$90,ObjID_blaster,$00,$11
+	fcb   $0B,$90,ObjID_blaster,$00,$04
+;$0B,$92,ObjID_6,$00,$03
+;$0B,$9E,ObjID_6,$00,$06
+;$0B,$B8,ObjID_6,$00,$03
+	fcb   $0B,$D0,ObjID_blaster,$00,$01
+	fcb   $0B,$D0,ObjID_blaster,$00,$04
+;$0B,$DE,ObjID_6,$00,$16
+	fcb   $0B,$E4,ObjID_bink,$00,$07
+;$0B,$FC,ObjID_6,$00,$03
+;$0C,$0C,ObjID_6,$00,$06
+	fcb   $0C,$10,ObjID_blaster,$00,$12
+	fcb   $0C,$10,ObjID_blaster,$00,$03
+	fcb   $0C,$50,ObjID_blaster,$00,$02
+	fcb   $0C,$50,ObjID_blaster,$00,$03
+;$0C,$5C,ObjID_6,$00,$24
+	fcb   $0C,$5E,ObjID_bink,$00,$17
+	fcb   $0C,$80,ObjID_fadetotunnel,$00,$00
+;$0C,$F8,ObjID_6,$00,$27
+;$0C,$FC,ObjID_21,$00,$08
 	fcb   $0D,$88,ObjID_pow,$00,$35
-        fcb   $0E,$10,ObjID_blaster,$00,$00
+	fcb   $0E,$10,ObjID_blaster,$00,$00
 	fcb   $0E,$2C,ObjID_pow,$00,$04
-        fcb   $0E,$50,ObjID_blaster,$00,$00
 	fcb   $0E,$50,ObjID_bug,$01,$22
-	fcb   $0E,$80,ObjID_shell,$00,$00
-
-        fcb   $11,$10,ObjID_blaster,$00,$01
-        fcb   $11,$50,ObjID_blaster,$00,$01
-        fcb   $11,$90,ObjID_blaster,$00,$10
-        fcb   $11,$D0,ObjID_blaster,$00,$00
+	fcb   $0E,$50,ObjID_blaster,$00,$00
+	fcb   $0E,$58,ObjID_shell,$00,$00
+;$0E,$80,ObjID_32,$00,$00
+	fcb   $11,$10,ObjID_blaster,$00,$01
+	fcb   $11,$50,ObjID_blaster,$00,$01
+	fcb   $11,$5C,ObjID_bink,$00,$27
+	fcb   $11,$90,ObjID_blaster,$00,$10
+	fcb   $11,$D0,ObjID_blaster,$00,$00
 	fcb   $11,$DA,ObjID_patapata,$00,$2A
 	fcb   $11,$EC,ObjID_patapata,$00,$29
 	fcb   $12,$0A,ObjID_patapata,$00,$3A
-        fcb   $12,$10,ObjID_blaster,$00,$10
-        fcb   $12,$50,ObjID_blaster,$00,$30
+	fcb   $12,$10,ObjID_blaster,$00,$10
+	fcb   $12,$50,ObjID_blaster,$00,$30
+;$12,$52,ObjID_21,$00,$08
 	fcb   $12,$60,ObjID_patapata,$00,$24
 	fcb   $12,$82,ObjID_pow,$00,$57
-        fcb   $12,$90,ObjID_blaster,$00,$00
+	fcb   $12,$90,ObjID_blaster,$00,$00
 	fcb   $12,$C4,ObjID_patapata,$00,$08
-        fcb   $12,$D0,ObjID_blaster,$00,$20
+	fcb   $12,$D0,ObjID_blaster,$00,$20
 	fcb   $13,$1E,ObjID_pow,$00,$47
-        fcb   $13,$90,ObjID_blaster,$00,$02
-        fcb   $13,$90,ObjID_blaster,$00,$03
+;$13,$80,ObjID_0,$00,$03
+	fcb   $13,$90,ObjID_blaster,$00,$02
+	fcb   $13,$90,ObjID_blaster,$00,$03
+	fcb   $13,$9E,ObjID_bink,$00,$16
+;$13,$C0,ObjID_32,$00,$01
 	fcb   $13,$C8,ObjID_bug,$02,$22
-        fcb   $13,$D0,ObjID_blaster,$00,$02
-        fcb   $13,$D0,ObjID_blaster,$00,$13
+	fcb   $13,$D0,ObjID_blaster,$00,$02
+	fcb   $13,$D0,ObjID_blaster,$00,$13
+	fcb   $14,$16,ObjID_bink,$00,$32
 	fcb   $14,$B8,ObjID_pow,$00,$75
-
-
-        fcb   $16,$10,ObjID_blaster,$00,$00
-        fcb   $16,$50,ObjID_blaster,$00,$00
-        fcb   $16,$90,ObjID_blaster,$00,$20
-        fcb   $16,$D0,ObjID_blaster,$00,$00
-        fcb   $17,$10,ObjID_blaster,$00,$01
-        fcb   $17,$50,ObjID_blaster,$00,$11
+	fcb   $16,$10,ObjID_blaster,$00,$00
+	fcb   $16,$50,ObjID_blaster,$00,$00
+	fcb   $16,$90,ObjID_blaster,$00,$20
+	fcb   $16,$D0,ObjID_blaster,$00,$00
+;$16,$D4,ObjID_11,$00,$07
+	fcb   $17,$10,ObjID_blaster,$00,$01
+	fcb   $17,$50,ObjID_blaster,$00,$11
 	fcb   $18,$22,ObjID_pow,$00,$04
-
-	fcb   $19,$80,ObjID_fadetotunnel,$01,$00
-
+;$18,$6A,ObjID_11,$00,$06
+	fcb   $19,$68,ObjID_fadetotunnel,$00,$01
+;$19,$68,ObjID_24,$00,$02
+;$1A,$30,ObjID_39,$00,$00
+;$1B,$40,ObjID_22,$00,$00
+;$1D,$14,ObjID_2,$00,$00
+;$1D,$80,ObjID_26,$00,$00
+	fcb   $1D,$FC,ObjID_bossmusic,$00,$00
 	fdb   $FFFF
-
-
