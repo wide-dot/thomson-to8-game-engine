58282aeb8ee4c13ea1a4dce41d5c3c1858700786
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-27 10:15:29 +0200

    All level wave data + lvl07 properties update for patapata and bugs

--- game-projects/r-type/objects/levels/07/object-wave/object-wave-data.asm
+++ game-projects/r-type/objects/levels/07/object-wave/object-wave-data.asm
@@ -1,49 +1,178 @@
-;$00,$00,ObjID_25,$00,$04
+;$00,$00,ObjID_25,$00,$0D
 ;$00,$00,ObjID_1,$00,$00
-;$00,$C8,ObjID_13,$00,$0A
-;$01,$90,ObjID_13,$00,$00
-	;fcb   $02,$06,ObjID_pow,$00,$35 ; original data
-        fcb   $02,$06,ObjID_pow,$00,$45 ; test (to remove)
-        fcb   $02,$0A,ObjID_pow,$00,$45 ; test (to remove)
-        fcb   $02,$0E,ObjID_pow,$00,$45 ; test (to remove)
-;$02,$58,ObjID_13,$00,$01
-;$03,$20,ObjID_13,$00,$07
-;$03,$D4,ObjID_13,$00,$01
-;$04,$4C,ObjID_13,$00,$02
-;$04,$56,ObjID_13,$00,$00
-;$04,$60,ObjID_13,$00,$0E
-;$05,$3C,ObjID_13,$00,$07
-;$05,$3C,ObjID_13,$00,$05
-;$05,$C8,ObjID_13,$00,$02
-;$06,$40,ObjID_13,$00,$01
-;$06,$54,ObjID_15,$00,$00
-;$06,$68,ObjID_13,$00,$03
-;$06,$E0,ObjID_36,$00,$01
-;$07,$08,ObjID_13,$00,$00
-;$07,$DA,ObjID_13,$00,$03
-;$07,$E4,ObjID_13,$00,$0C
-	;fcb   $08,$20,ObjID_pow,$00,$14
-;$08,$34,ObjID_13,$00,$03
-;$08,$B4,ObjID_36,$00,$00
-;$09,$10,ObjID_13,$00,$00
-;$09,$4C,ObjID_13,$00,$02
-;$0A,$3C,ObjID_13,$00,$01
-;$0A,$50,ObjID_13,$00,$04
-;$0A,$80,ObjID_0,$00,$05
-;$0A,$BC,ObjID_13,$00,$07
-;$0A,$F0,ObjID_13,$00,$01
-;$0B,$98,ObjID_13,$00,$02
-;$0C,$58,ObjID_13,$00,$02
-;$0D,$48,ObjID_13,$00,$03
-	;fcb   $0D,$B4,ObjID_pow,$00,$06
-;$0E,$10,ObjID_13,$00,$02
-;$0E,$10,ObjID_13,$00,$00
-;$0E,$7C,ObjID_13,$00,$01
-;$0E,$88,ObjID_29,$00,$04
-	;fcb   $0F,$64,ObjID_pow,$00,$07
-	fcb   $13,$60,ObjID_bossmusic,$00,$00
-;$13,$60,ObjID_39,$00,$00
-;$14,$80,ObjID_28,$00,$00
-;$14,$FC,ObjID_40,$00,$00
-
+;$00,$30,ObjID_32,$00,$02
+	fcb   $00,$7C,ObjID_patapata,$00,$47
+	fcb   $00,$8C,ObjID_patapata,$00,$38
+	fcb   $00,$98,ObjID_patapata,$00,$47
+	fcb   $00,$A0,ObjID_pow,$00,$36
+	fcb   $00,$C2,ObjID_patapata,$00,$57
+	fcb   $00,$DC,ObjID_patapata,$00,$38
+	fcb   $00,$F4,ObjID_patapata,$00,$47
+	fcb   $01,$08,ObjID_patapata,$00,$38
+	fcb   $01,$20,ObjID_patapata,$00,$37
+	fcb   $01,$48,ObjID_patapata,$00,$47
+;$01,$48,ObjID_16,$00,$05
+	fcb   $01,$66,ObjID_patapata,$00,$38
+	fcb   $01,$8E,ObjID_patapata,$00,$57
+;$01,$A4,ObjID_21,$00,$08
+;$01,$C8,ObjID_16,$00,$05
+	fcb   $01,$D2,ObjID_patapata,$00,$48
+	fcb   $02,$06,ObjID_patapata,$00,$37
+	fcb   $02,$44,ObjID_patapata,$00,$58
+;$02,$44,ObjID_6,$00,$44
+;$02,$48,ObjID_16,$00,$05
+;$02,$48,ObjID_16,$00,$04
+;$02,$58,ObjID_6,$00,$54
+;$02,$6C,ObjID_21,$00,$08
+;$02,$7E,ObjID_6,$00,$4F
+;$02,$80,ObjID_5,$00,$46
+;$02,$80,ObjID_5,$00,$58
+;$02,$C8,ObjID_16,$00,$04
+	fcb   $02,$DE,ObjID_pow,$00,$75
+;$03,$0C,ObjID_6,$00,$4E
+;$03,$34,ObjID_6,$00,$3E
+;$03,$3C,ObjID_5,$00,$65
+	fcb   $03,$50,ObjID_bug,$02,$22
+	fcb   $03,$92,ObjID_pow,$00,$07
+;$03,$D0,ObjID_21,$00,$08
+;$03,$D0,ObjID_18,$00,$3A
+;$03,$D4,ObjID_6,$00,$44
+;$03,$E8,ObjID_18,$00,$3A
+;$04,$08,ObjID_16,$00,$01
+;$04,$18,ObjID_18,$00,$37
+;$04,$3A,ObjID_5,$00,$56
+;$04,$46,ObjID_18,$00,$3A
+;$04,$72,ObjID_6,$00,$41
+;$04,$74,ObjID_6,$00,$37
+;$04,$78,ObjID_16,$00,$03
+;$04,$88,ObjID_16,$00,$00
+;$04,$EC,ObjID_5,$00,$46
+;$04,$F8,ObjID_16,$00,$02
+;$05,$1A,ObjID_6,$00,$30
+;$05,$48,ObjID_16,$00,$05
+	fcb   $05,$48,ObjID_pow,$00,$0E
+;$05,$64,ObjID_5,$00,$54
+;$05,$F0,ObjID_38,$00,$00
+;$06,$08,ObjID_16,$00,$01
+;$06,$48,ObjID_16,$00,$04
+;$06,$68,ObjID_18,$00,$47
+;$06,$A0,ObjID_16,$00,$03
+;$06,$A2,ObjID_18,$00,$37
+;$06,$E0,ObjID_5,$00,$46
+;$06,$E0,ObjID_5,$00,$58
+;$07,$48,ObjID_16,$00,$05
+;$07,$6C,ObjID_5,$00,$63
+;$07,$6C,ObjID_5,$00,$56
+;$07,$6C,ObjID_5,$00,$48
+;$07,$B8,ObjID_5,$00,$53
+;$07,$C8,ObjID_16,$00,$05
+;$07,$C8,ObjID_16,$00,$04
+;$07,$D0,ObjID_5,$00,$33
+	fcb   $08,$38,ObjID_pow,$00,$45
+;$08,$4E,ObjID_6,$00,$35
+;$08,$88,ObjID_16,$00,$01
+;$08,$88,ObjID_16,$00,$00
+;$08,$98,ObjID_19,$00,$07
+;$08,$D4,ObjID_19,$00,$07
+;$08,$E8,ObjID_5,$00,$45
+;$08,$F8,ObjID_16,$00,$02
+;$08,$F8,ObjID_16,$00,$03
+;$09,$50,ObjID_21,$00,$08
+;$09,$C8,ObjID_16,$00,$05
+;$09,$D8,ObjID_5,$01,$3E
+;$0A,$0C,ObjID_6,$00,$5F
+;$0A,$26,ObjID_6,$00,$3E
+;$0A,$48,ObjID_16,$00,$05
+;$0A,$48,ObjID_6,$00,$33
+;$0A,$8C,ObjID_6,$00,$45
+;$0A,$B2,ObjID_6,$00,$3C
+;$0A,$C8,ObjID_16,$00,$05
+;$0A,$DC,ObjID_6,$00,$48
+;$0B,$00,ObjID_0,$00,$0E
+;$0B,$30,ObjID_32,$00,$03
+;$0B,$48,ObjID_5,$00,$48
+;$0B,$70,ObjID_5,$00,$38
+;$0B,$8C,ObjID_5,$01,$4E
+;$0B,$98,ObjID_5,$00,$48
+;$0B,$C0,ObjID_5,$00,$38
+;$0B,$E8,ObjID_5,$00,$38
+;$0C,$10,ObjID_5,$00,$58
+;$0C,$38,ObjID_5,$00,$48
+;$0C,$48,ObjID_16,$00,$04
+;$0C,$60,ObjID_5,$00,$38
+;$0C,$80,ObjID_5,$00,$38
+;$0C,$80,ObjID_6,$00,$33
+;$0C,$BC,ObjID_5,$00,$38
+;$0C,$C8,ObjID_16,$00,$04
+;$0D,$0E,ObjID_6,$00,$38
+	fcb   $0D,$30,ObjID_bug,$02,$12
+;$0D,$48,ObjID_5,$00,$55
+	fcb   $0D,$70,ObjID_bug,$03,$02
+;$0D,$70,ObjID_6,$00,$3E
+	fcb   $0D,$96,ObjID_pow,$00,$38
+;$0D,$98,ObjID_6,$00,$4F
+;$0D,$C8,ObjID_16,$00,$05
+;$0D,$D4,ObjID_6,$00,$53
+;$0D,$E8,ObjID_6,$00,$56
+;$0E,$0E,ObjID_6,$00,$54
+;$0F,$28,ObjID_21,$00,$08
+;$0F,$48,ObjID_16,$00,$05
+;$0F,$88,ObjID_21,$00,$08
+	fcb   $0F,$92,ObjID_patapata,$00,$64
+	fcb   $0F,$B4,ObjID_patapata,$00,$46
+;$0F,$C8,ObjID_16,$00,$04
+;$0F,$C8,ObjID_16,$00,$05
+;$0F,$D4,ObjID_21,$00,$08
+	fcb   $0F,$F6,ObjID_patapata,$00,$46
+	fcb   $10,$10,ObjID_patapata,$00,$04
+;$10,$14,ObjID_21,$00,$08
+	fcb   $10,$22,ObjID_patapata,$00,$47
+	fcb   $10,$30,ObjID_patapata,$00,$83
+	fcb   $10,$48,ObjID_patapata,$00,$05
+;$10,$48,ObjID_16,$00,$05
+	fcb   $10,$66,ObjID_patapata,$00,$06
+	fcb   $10,$7C,ObjID_patapata,$00,$04
+;$10,$88,ObjID_21,$00,$08
+	fcb   $10,$AE,ObjID_patapata,$00,$53
+	fcb   $10,$B6,ObjID_pow,$00,$54
+	fcb   $10,$D6,ObjID_patapata,$00,$07
+	fcb   $10,$E8,ObjID_patapata,$00,$75
+	fcb   $10,$FA,ObjID_patapata,$00,$04
+	fcb   $00,$00,ObjID_patapata,$00,$00
+	fcb   $00,$00,ObjID_patapata,$00,$00
+	fcb   $11,$4A,ObjID_patapata,$00,$73
+	fcb   $11,$58,ObjID_patapata,$00,$54
+	fcb   $11,$74,ObjID_patapata,$00,$06
+	fcb   $11,$88,ObjID_patapata,$00,$74
+;$11,$A0,ObjID_42,$00,$00
+	fcb   $11,$A0,ObjID_patapata,$00,$37
+	fcb   $11,$B4,ObjID_patapata,$00,$42
+	fcb   $11,$C2,ObjID_patapata,$00,$04
+	fcb   $11,$DE,ObjID_patapata,$00,$45
+	fcb   $11,$E8,ObjID_patapata,$00,$57
+	fcb   $11,$FC,ObjID_patapata,$00,$03
+	fcb   $12,$14,ObjID_patapata,$00,$06
+	fcb   $12,$28,ObjID_patapata,$00,$53
+	fcb   $12,$3A,ObjID_patapata,$00,$54
+	fcb   $12,$4E,ObjID_patapata,$00,$65
+	fcb   $12,$62,ObjID_patapata,$00,$02
+	fcb   $12,$7A,ObjID_patapata,$00,$56
+;$12,$8E,ObjID_5,$01,$4C
+	fcb   $12,$96,ObjID_patapata,$00,$66
+	fcb   $12,$AA,ObjID_patapata,$00,$43
+;$12,$C0,ObjID_39,$00,$00
+	fcb   $12,$D8,ObjID_patapata,$00,$55
+	fcb   $12,$E2,ObjID_patapata,$00,$68
+	fcb   $12,$EC,ObjID_patapata,$00,$24
+	fcb   $13,$00,ObjID_patapata,$00,$42
+	fcb   $13,$0C,ObjID_patapata,$00,$55
+	fcb   $13,$16,ObjID_patapata,$00,$57
+	fcb   $13,$30,ObjID_patapata,$00,$55
+	fcb   $13,$44,ObjID_patapata,$00,$63
+	fcb   $13,$58,ObjID_patapata,$00,$16
+	fcb   $13,$6C,ObjID_patapata,$00,$64
+	fcb   $13,$76,ObjID_patapata,$00,$43
+;$13,$88,ObjID_37,$00,$06
+;$14,$88,ObjID_26,$00,$00
+	fcb   $14,$FC,ObjID_bossmusic,$00,$00
 	fdb   $FFFF
