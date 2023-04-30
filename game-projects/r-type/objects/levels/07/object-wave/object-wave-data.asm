fd604ac201639dcfb0c4a06680d7e1cb02c85061
yoanrhk <122287689+yoanrhk@users.noreply.github.com>
2023-04-30 09:17:28 +0200

    Bink fix and in stage 7

--- game-projects/r-type/objects/levels/07/object-wave/object-wave-data.asm
+++ game-projects/r-type/objects/levels/07/object-wave/object-wave-data.asm
@@ -25,13 +25,13 @@
 ;$02,$58,ObjID_6,$00,$54
 ;$02,$6C,ObjID_21,$00,$08
 ;$02,$7E,ObjID_6,$00,$4F
-	;fcb   $02,$80,ObjID_bink,$00,$46
-	;fcb   $02,$80,ObjID_bink,$00,$58
+	fcb   $02,$80,ObjID_bink,$00,$46
+	fcb   $02,$80,ObjID_bink,$00,$58
 ;$02,$C8,ObjID_16,$00,$04
 	fcb   $02,$DE,ObjID_pow,$00,$75
 ;$03,$0C,ObjID_6,$00,$4E
 ;$03,$34,ObjID_6,$00,$3E
-	;fcb   $03,$3C,ObjID_bink,$00,$65
+	fcb   $03,$3C,ObjID_bink,$00,$65
 	fcb   $03,$50,ObjID_bug,$02,$22
 	fcb   $03,$92,ObjID_pow,$00,$07
 ;$03,$D0,ObjID_21,$00,$08
@@ -40,46 +40,46 @@
 ;$03,$E8,ObjID_18,$00,$3A
 ;$04,$08,ObjID_16,$00,$01
 ;$04,$18,ObjID_18,$00,$37
-	;fcb   $04,$3A,ObjID_bink,$00,$56
+	fcb   $04,$3A,ObjID_bink,$00,$56
 ;$04,$46,ObjID_18,$00,$3A
 ;$04,$72,ObjID_6,$00,$41
 ;$04,$74,ObjID_6,$00,$37
 ;$04,$78,ObjID_16,$00,$03
 ;$04,$88,ObjID_16,$00,$00
-	;fcb   $04,$EC,ObjID_bink,$00,$46
+	fcb   $04,$EC,ObjID_bink,$00,$46
 ;$04,$F8,ObjID_16,$00,$02
 ;$05,$1A,ObjID_6,$00,$30
 ;$05,$48,ObjID_16,$00,$05
 	fcb   $05,$48,ObjID_pow,$00,$0E
-	;fcb   $05,$64,ObjID_bink,$00,$54
+	fcb   $05,$64,ObjID_bink,$00,$54
 ;$05,$F0,ObjID_38,$00,$00
 ;$06,$08,ObjID_16,$00,$01
 ;$06,$48,ObjID_16,$00,$04
 ;$06,$68,ObjID_18,$00,$47
 ;$06,$A0,ObjID_16,$00,$03
 ;$06,$A2,ObjID_18,$00,$37
-	;fcb   $06,$E0,ObjID_bink,$00,$46
-	;fcb   $06,$E0,ObjID_bink,$00,$58
+	fcb   $06,$E0,ObjID_bink,$00,$46
+	fcb   $06,$E0,ObjID_bink,$00,$58
 ;$07,$48,ObjID_16,$00,$05
-	;fcb   $07,$6C,ObjID_bink,$00,$63
-	;fcb   $07,$6C,ObjID_bink,$00,$56
-	;fcb   $07,$6C,ObjID_bink,$00,$48
-	;fcb   $07,$B8,ObjID_bink,$00,$53
+	fcb   $07,$6C,ObjID_bink,$00,$63
+	fcb   $07,$6C,ObjID_bink,$00,$56
+	fcb   $07,$6C,ObjID_bink,$00,$48
+	fcb   $07,$B8,ObjID_bink,$00,$53
 ;$07,$C8,ObjID_16,$00,$05
 ;$07,$C8,ObjID_16,$00,$04
-	;fcb   $07,$D0,ObjID_bink,$00,$33
+	fcb   $07,$D0,ObjID_bink,$00,$33
 	fcb   $08,$38,ObjID_pow,$00,$45
 ;$08,$4E,ObjID_6,$00,$35
 ;$08,$88,ObjID_16,$00,$01
 ;$08,$88,ObjID_16,$00,$00
 ;$08,$98,ObjID_19,$00,$07
 ;$08,$D4,ObjID_19,$00,$07
-	;fcb   $08,$E8,ObjID_bink,$00,$45
+	fcb   $08,$E8,ObjID_bink,$00,$45
 ;$08,$F8,ObjID_16,$00,$02
 ;$08,$F8,ObjID_16,$00,$03
 ;$09,$50,ObjID_21,$00,$08
 ;$09,$C8,ObjID_16,$00,$05
-	;fcb   $09,$D8,ObjID_bink,$01,$3E
+	fcb   $09,$D8,ObjID_bink,$01,$3E
 ;$0A,$0C,ObjID_6,$00,$5F
 ;$0A,$26,ObjID_6,$00,$3E
 ;$0A,$48,ObjID_16,$00,$05
@@ -90,23 +90,23 @@
 ;$0A,$DC,ObjID_6,$00,$48
 ;$0B,$00,ObjID_0,$00,$0E
 ;$0B,$30,ObjID_32,$00,$03
-	;fcb   $0B,$48,ObjID_bink,$00,$48
-	;fcb   $0B,$70,ObjID_bink,$00,$38
-	;fcb   $0B,$8C,ObjID_bink,$01,$4E
-	;fcb   $0B,$98,ObjID_bink,$00,$48
-	;fcb   $0B,$C0,ObjID_bink,$00,$38
-	;fcb   $0B,$E8,ObjID_bink,$00,$38
-	;fcb   $0C,$10,ObjID_bink,$00,$58
-	;fcb   $0C,$38,ObjID_bink,$00,$48
+	fcb   $0B,$48,ObjID_bink,$00,$48
+	fcb   $0B,$70,ObjID_bink,$00,$38
+	fcb   $0B,$8C,ObjID_bink,$01,$4E
+	fcb   $0B,$98,ObjID_bink,$00,$48
+	fcb   $0B,$C0,ObjID_bink,$00,$38
+	fcb   $0B,$E8,ObjID_bink,$00,$38
+	fcb   $0C,$10,ObjID_bink,$00,$58
+	fcb   $0C,$38,ObjID_bink,$00,$48
 ;$0C,$48,ObjID_16,$00,$04
-	;fcb   $0C,$60,ObjID_bink,$00,$38
-	;fcb   $0C,$80,ObjID_bink,$00,$38
+	fcb   $0C,$60,ObjID_bink,$00,$38
+	fcb   $0C,$80,ObjID_bink,$00,$38
 ;$0C,$80,ObjID_6,$00,$33
-	;fcb   $0C,$BC,ObjID_bink,$00,$38
+	fcb   $0C,$BC,ObjID_bink,$00,$38
 ;$0C,$C8,ObjID_16,$00,$04
 ;$0D,$0E,ObjID_6,$00,$38
 	fcb   $0D,$30,ObjID_bug,$02,$12
-	;fcb   $0D,$48,ObjID_bink,$00,$55
+	fcb   $0D,$48,ObjID_bink,$00,$55
 	fcb   $0D,$70,ObjID_bug,$03,$02
 ;$0D,$70,ObjID_6,$00,$3E
 	fcb   $0D,$96,ObjID_pow,$00,$38
@@ -157,7 +157,7 @@
 	fcb   $12,$4E,ObjID_patapata,$00,$65
 	fcb   $12,$62,ObjID_patapata,$00,$02
 	fcb   $12,$7A,ObjID_patapata,$00,$56
-	;fcb   $12,$8E,ObjID_bink,$01,$4C
+	fcb   $12,$8E,ObjID_bink,$01,$4C
 	fcb   $12,$96,ObjID_patapata,$00,$66
 	fcb   $12,$AA,ObjID_patapata,$00,$43
 ;$12,$C0,ObjID_39,$00,$00
