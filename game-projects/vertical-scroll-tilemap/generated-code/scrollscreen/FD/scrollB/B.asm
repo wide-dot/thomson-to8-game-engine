d4fa87d344d738ee30bb1ddfa68a5f271748f631
François-Xavier Robin <fxrobin@gmail.com>
2023-05-01 22:36:35 +0200

    vscroll avec taille buffer paramétrable via EQU et IFNDEF

--- game-projects/vertical-scroll-tilemap/generated-code/scrollscreen/FD/scrollB/B.asm
+++
@@ -1,9 +0,0 @@
-	opt   c,ct
-	INCLUDE "./generated-code/scrollscreen/FD/main.glb"
-	org   $0000
-	setdp $FF
-
-        
-@loop
-        INCLUDEBIN "./objects/scroll/small-map-indexed.0.0.bin.vscroll"
-        jmp   @loop
