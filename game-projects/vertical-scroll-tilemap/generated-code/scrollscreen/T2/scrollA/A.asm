aeb25e4eb7c519d8dabed9594909f46584b5dd05
François-Xavier Robin <fxrobin@gmail.com>
2023-05-02 15:29:28 +0200

    nettoyage + gitignore

--- game-projects/vertical-scroll-tilemap/generated-code/scrollscreen/T2/scrollA/A.asm
+++
@@ -1,9 +0,0 @@
-	opt   c,ct
-	INCLUDE "./generated-code/scrollscreen/T2/main.glb"
-	org   $017F
-	setdp $FF
-
-        
-@loop
-        INCLUDEBIN "./objects/scroll/small-map-indexed.1.0.bin.vscroll"
-        jmp   @loop
