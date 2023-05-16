8fdd43e97011550edf2b5376dc24a01a7f9576ae
bhrousseau <56814489+bhrousseau@users.noreply.github.com>
2023-05-16 07:37:08 +0200

    pow wip

--- game-projects/r-type/objects/animation/obj.asm
+++ game-projects/r-type/objects/animation/obj.asm
@@ -5,9 +5,11 @@
         fdb   animidx_192EC ; bug
         fdb   animidx_19A96 ; pow
         fdb   animidx_19AB2 ; pow
+        fdb   animidx_19AA2 ; pow
 
         INCLUDE "./objects/animation/anim-data.asm"
         INCLUDE "./objects/animation/patapata.asm"
         INCLUDE "./objects/animation/bug.asm"
         INCLUDE "./objects/animation/pow_0.asm"
         INCLUDE "./objects/animation/pow_1.asm"
+        INCLUDE "./objects/animation/pow_2.asm"
