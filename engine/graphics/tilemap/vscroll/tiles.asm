5439c1e61c278c6f01d628ff56f8be37ec1ecc5d
bhrou <bhrou@Goliath>
2024-12-22 19:21:30 +0100

    add vscroll.tiles.draw position computing

--- engine/graphics/tilemap/vscroll/tiles.asm
+++ engine/graphics/tilemap/vscroll/tiles.asm
@@ -6,7 +6,8 @@
 ; Add a sprite (based on tile mapping) to a drawing queue.
 ; With video double buffering, we need to maintain two arrays.
 ; -----------------------------------------------------------------------------
-; input  REG : [d] x,y coordinates of top left corner for sprite
+; input  REG : [b] x absolute coordinate in pixels
+;              [x] y absolute coordinate in pixels
 ;              [y] ptr to sprite tile mapping data
 ; -----------------------------------------------------------------------------
 
@@ -14,7 +15,7 @@
 vscroll.tiles.nbMaxUpdates equ 16 ; by default 16 sprites can be added to tile update list
  ENDC
 
-vscroll.tiles.bufferElementSize equ 4
+vscroll.tiles.bufferElementSize equ 5
 
 vscroll.tiles.buffer
         fdb   vscroll.tiles.bufferA
@@ -26,9 +27,33 @@ vscroll.tiles.bufferA
 vscroll.tiles.bufferB
         fill  0,(vscroll.tiles.nbMaxUpdates*vscroll.tiles.bufferElementSize+2)
 
-
 vscroll.tiles.draw
-        std   @coordinates
+        ; transform sprite position in tile position
+        lsrb
+        lsrb
+        lsrb
+        stb   @x
+        tfr   x,d
+        _lsrd
+        _lsrd
+        _lsrd
+        _lsrd
+        std   @y
+        ; adjust position based on image size
+        ldb   ,y ; load width in tiles
+        decb
+        lsrb
+        negb
+        addb  @x
+        stb   @x
+        clra
+        ldb   1,y ; load height in tiles
+        decb
+        lsrb
+        _negd
+        addd  @y
+        std   @y
+        ; copy data to draw list
         ldb   gfxlock.backBuffer.id ; alternate buffer based on in/active double buffering RAM
         aslb
         ldx   #vscroll.tiles.buffer
@@ -36,20 +61,23 @@ vscroll.tiles.draw
         ldx   ,x
         ; update size of array
         ldd   ,x
-        addd  #4
+        addd  #vscroll.tiles.bufferElementSize
         cmpd  #vscroll.tiles.nbMaxUpdates*vscroll.tiles.bufferElementSize
         bls   >
         orcc  #%00000001 ; return KO
         rts              ; array is full
 !       std   ,x
-        subd  #2  ; skip header (size data)
+        subd  #vscroll.tiles.bufferElementSize-2  ; skip header (size data)
         leax  d,x ; go to first free element of the array
+        ldb   #0
+@x      equ   *-1
+        stb   ,x  ; add x coordinates to array
         ldd   #0
-@coordinates equ *-2
-        std   ,x  ; add coordinates to array
-        sty   2,x ; add ptr (sprite tile mapping data) to array
+@y      equ    *-2
+        std   1,x ; add y coordinates to array
+        sty   3,x ; add ptr (sprite tile mapping data) to array
         andcc #%11111110
-        rts              ; return OK
+        rts       ; return OK
 
 vscroll.tiles.resetFrame
         ldb   gfxlock.backBuffer.id ; alternate buffer based on in/active double buffering RAM
