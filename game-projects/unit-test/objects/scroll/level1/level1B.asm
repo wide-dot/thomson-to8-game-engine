c9edd180cda34c9f1d7770f964d2d95985af37e0
bhrou <bhrou@Goliath>
2025-01-01 17:28:26 +0100

    fixed vscroll and unit tests

--- game-projects/unit-test/objects/scroll/level1/level1B.asm
+++ game-projects/unit-test/objects/scroll/level1/level1B.asm
@@ -1,5 +1,5 @@
         INCLUDE "./engine/graphics/tilemap/vscroll/vscroll.macro.asm"
 @loop
         INCLUDEBIN "./objects/scroll/level1/level1.start.1.0.bin.vscroll"
-        _vscroll.buffer.line
+        _vscroll.buffer.linex8
         jmp   @loop
--- game-projects/unit-test/objects/scroll/level2/levelbufferA.asm
+++ game-projects/unit-test/objects/scroll/level2/levelbufferA.asm
@@ -1,5 +1,5 @@
     INCLUDE "./engine/graphics/tilemap/vscroll/vscroll.macro.asm"
 @loop
         INCLUDEBIN "./objects/scroll/level2/level.start.0.vscroll"
-        _vscroll.buffer.line
+        _vscroll.buffer.linex8
         jmp   @loop
